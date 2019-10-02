783f54246b8620733523427a50e2f7d83e5ce63c
JeonJe <43032391+JeonJe@users.noreply.github.com>
2019-10-02 16:48:50 +0900

    프로그래머스 문제풀이

---
+++ 프로그래머스/고득점 kit_스택_큐/프로그래머스_쇠막대기_스택_큐.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <stack>
+using namespace std;
+
+
+int solution(string arrangement) {
+
+	stack<char> s;
+	int answer = 0;
+
+	for (int i = 0; i < (int) arrangement.size(); ++i) {
+		if (arrangement[i] == '(')  //쇠파이프 수 증가 
+			s.push('('); 
+		
+		else {
+			s.pop(); 
+			if (arrangement[i-1] == ')')
+				answer += 1; // 다음으로 ')' 이면 이전 문자가 쇠파이프 끝 부분 추가 +1
+		
+			else if (arrangement[i-1] == '(')
+				answer += (int) s.size(); // 다음으로 ')' 만날 시 이전 문자가  '(' 이면 -> 레이저 stack 사이즈만큼 +
+										// 레이저로 쇠파이프 개수만큼 분리 
+			
+		}
+	}
+	return answer;
+}
+
+
+int main()
+{
+	string str = "(((()(()()))(())()))(()())";
+	cout << solution(str);
+	return 0;
+}
---
+++ 프로그래머스/고득점 kit_스택_큐/프로그래머스_주식가격_스택_큐.cpp
@@ -0,0 +1,36 @@
+#include <string>
+#include <vector>
+#include <iostream>
+#include <stack>
+
+using namespace std;
+
+vector<int> solution(vector<int> prices) {
+	vector<int> answer(prices.size());
+	
+	for (int i = 0; i < prices.size(); ++i) { //현재 주식 가격
+		int cnt = 0;
+		for (int j = i+1; j < prices.size(); ++j) { //현재 다음의 주식 가격들
+			if (prices[i] <= prices[j]) {
+				answer[i]++; //가격이 하락하지 않은 경우
+			}
+			else { //가격이 하락할 경우 
+				answer[i]++;
+				break;
+			}
+		}
+	}
+	
+	return answer;
+}
+
+int main()
+{
+	vector<int> p({ 1,2,3,2,3 });
+	vector<int> result = solution(p);
+
+	for (int i = 0; i < result.size(); i++)
+		cout << result[i] << " ";
+
+	return 0;
+}
