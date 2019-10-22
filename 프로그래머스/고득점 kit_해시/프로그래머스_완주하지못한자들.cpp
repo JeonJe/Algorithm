068ff75c4c8656409de4bcb725ad44b6f85e2444
jeonje <43032391+jeonje@users.noreply.github.com>
2019-10-22 17:48:43 +0900

    v5

--- 프로그래머스/고득점 kit_해시/프로그래머스_완주하지못한자들.cpp
+++ 프로그래머스/고득점 kit_해시/프로그래머스_완주하지못한자들.cpp
@@ -4,14 +4,16 @@
 using namespace std;
 
 string solution(vector<string> participant, vector<string> completion) {
-	vector <string>::iterator it;
-	for (int i = 0; i < completion.size(); i++) {
-		it = find(participant.begin(), participant.end(), completion.at(i));
-		if (it != participant.end()) it = participant.erase(it);
-		
+	string answer = "";
+	sort(participant.begin(), participant.end()); // 참가자 오름차순 정렬
+	sort(completion.begin(), completion.end()); // 완주자 오름차순 정렬
+
+	for (int i = 0; i < completion.size(); i++) {  //완주자 명단 확인
+		if (participant[i] != completion[i]) { // 참가자 명단이랑 완주자 명단이랑 다르다면
+											//(오름차순정렬) 이고 크기가 1차이
+			return participant[i];          //참가자에 있는 사람이 완주하지 못한 것
+		}
 	}
-	string answer="";
 
-	return participant[0];
+	return participant[participant.size() - 1];   // 정렬된 참가자 중 마지막 참가자가 완주하지 못함
 }
-
