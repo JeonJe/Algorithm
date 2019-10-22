eb271e55bf6525f4da06914b4aa00d13c6402884
jeonje <43032391+jeonje@users.noreply.github.com>
2019-10-22 12:12:27 +0900

    스택

--- 프로그래머스/고득점 kit_스택_큐/프로그래머스_기능개발_스택_큐.cpp
+++ 프로그래머스/고득점 kit_스택_큐/프로그래머스_기능개발_스택_큐.cpp
@@ -1,3 +1,5 @@
+//https://programmers.co.kr/learn/courses/30/lessons/42586
+
 #include <string>
 #include <vector>
 #include <iostream>
@@ -15,14 +17,14 @@ vector<int> solution(vector<int> progresses, vector<int> speeds) {
 
 		if (front == 100) { // 먼저 배포되어야 하는 것이 개발이 다 되었으면
 			int cnt = 0;
-			for (int i = 0; i < qsize; ) {
-				if (q.empty() || q[i] != 100)  { // 모두 배포되었거나 배포순서가 맞지 않으면
+			while (true) {
+				if (q.empty() || q[0] != 100)  { // 모두 배포되었거나 배포순서가 맞지 않으면
 					answer.emplace_back(cnt); // 배포된 기능 수 추가 
 					break;
 				}
 	
-				q.erase(q.begin()+i); // 순차대로 배포중이면 큐에서 삭제
-  				sq.erase(sq.begin()+i);
+				q.erase(q.begin()); // 순차대로 배포중이면 큐에서 삭제
+				sq.erase(sq.begin());
 				cnt++; // 순차적으로 몇개가 배포되는지 체크
 			}
 		}
