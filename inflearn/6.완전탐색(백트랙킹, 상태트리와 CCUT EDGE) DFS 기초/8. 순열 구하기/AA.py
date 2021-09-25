9eb53ed9bc42b480373d53347b99d8beecf80114
whssodi <whssodi@gamil.com>
2021-09-25 15:36:36 +0900

    210925

--- inflearn/6.완전탐색(백트랙킹, 상태트리와 CCUT EDGE) DFS 기초/8. 순열 구하기/AA.py
+++ inflearn/6.완전탐색(백트랙킹, 상태트리와 CCUT EDGE) DFS 기초/8. 순열 구하기/AA.py
@@ -4,20 +4,24 @@ import sys
 def DFS(L):
     global cnt
     if L==M:
-        if len(set(res))==M:
-            for i in res:   
-                print(i,end=' ')
-            print()
-            cnt+=1
-        return
+        for i in res:   
+            print(i,end=' ')
+        print()
+        cnt+=1
+        return 
+           
     else:
-        for i in range(N):
-            res[L] = i+1
-            DFS(L+1)
-            
+        for i in range(1,N+1):
+            if ch[i] == 0:
+                ch[i] =1
+                res[L] = i
+                DFS(L+1)
+                ch[i]=0
+  
 if __name__ == "__main__":
   N,M = map(int,input().split())
   cnt = 0
   res = [0]*M
+  ch = [0]*(N+1)
   DFS(0)
   print(cnt)
