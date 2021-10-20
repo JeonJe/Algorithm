6e7a0336c9b97639d27de3108d75b532c7c0d8aa
Jeon Je <whssodi@gmail.com>
2021-10-20 19:39:46 +0900

    211020

--- inflearn/4.이분 탐색(결정알고리즘)_그리디 알고리즘/6.씨름선수/AA.py
+++ inflearn/4.이분 탐색(결정알고리즘)_그리디 알고리즘/6.씨름선수/AA.py
@@ -1,23 +1,19 @@
 
 import sys
-# sys.stdin = open("input.txt",'rt')
+sys.stdin = open("input.txt",'rt')
 
 N = int(input())
 
-arr = [ list(map(int,input().split())) for _ in range(N)]
 
-arr.sort(key=lambda x : (-x[0],-x[1]))
+arr = [input() for _ in range(N)]
+arr2 = [input() for _ in range(N-1)]
 
-res=[]
- 
-res.append((arr[0][0], arr[0][1]))
+arr.sort()
+arr2.sort()
 
-for i in range(1,N):
-    #몸무게가 많이 나가는 순서대로 뽑는다.
-    candiate_height, cadidate_weight = arr[i][0],arr[i][1]
-    #남은지원자
-    if  cadidate_weight > max(res,key=lambda x : x[1])[1]:
-        res.append((candiate_height,cadidate_weight))
-    #단 몸무게가 적게 나가도 
+for i in range(N-1):
+    if arr[i] != arr2[i]:
+        break
 
-print(len(res))
+else:
+    print(arr[N])
