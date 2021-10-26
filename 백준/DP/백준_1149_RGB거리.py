77d5fc818f7c71fe80a366273e7940afc088fbae
Jeon Je <whssodi@gmail.com>
2021-10-26 09:43:17 +0900

    DP

--- 백준/DP/백준_1149_RGB거리.py
+++ 백준/DP/백준_1149_RGB거리.py
@@ -1,7 +1,6 @@
 
-
 import sys
-# sys.stdin = open("input.txt",'rt')
+sys.stdin = open("input.txt",'rt')
 N = int(input())
 
 arr = [ list(map(int,input().split())) for _ in range(N)]
@@ -15,4 +14,3 @@ for i in range(1,N):
     dp[i][2] = min(dp[i-1][0],dp[i-1][1]) + arr[i][2]
 
 print(min(dp[N-1][0],dp[N-1][1],dp[N-1][2]))
-    
