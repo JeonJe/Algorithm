63a15dfedc2398b17166cb92cbda83c12169fd96
2premise <43032391+JeonJe@users.noreply.github.com>
2022-11-05 16:17:29 +0900

    [Silver V] Title: 요세푸스 문제 0, Time: 148 ms, Memory: 115636 KB -BaekjoonHub

---
+++ 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.py
@@ -0,0 +1,23 @@
+
+from collections import deque
+
+N, K = map(int,input().split())
+
+que = deque()
+for i in range(N):
+    que.append(i+1)
+res = []
+idx = 1
+
+while len(que) > 1:
+    x = que.popleft()
+    if idx % K != 0:
+        que.append(x)
+    else:
+        res.append(x)
+        idx = 0
+    idx += 1
+
+res.append(que[0])
+r = ", ".join(map(str,res))
+print(f'<{r}>')
