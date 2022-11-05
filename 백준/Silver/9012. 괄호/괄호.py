f8955fc0617587c0cb5e526915127e8dedfb516c
2premise <43032391+JeonJe@users.noreply.github.com>
2022-11-05 11:00:06 +0900

    [Silver IV] Title: 괄호, Time: 112 ms, Memory: 114328 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.py
+++ 백준/Silver/9012. 괄호/괄호.py
@@ -1,19 +1,19 @@
 import sys 
-
 T = int(input())
-
 
 def VPS(target):
     for i in range(len(target)):
         if data[i] == "(":
             stack.append('(')
         elif data[i] == ")":
-            if len(stack) >= 1 and stack[-1] == "(":
+            if len(stack)>0 and stack[-1] == "(":
                 stack.pop()
             else:
                 return False
-    if len(stack) > 0: return False
-    else: return True 
+    if len(stack) > 0: 
+        return False
+    else: 
+        return True 
 
 for _ in range(T):
     data = sys.stdin.readline()
