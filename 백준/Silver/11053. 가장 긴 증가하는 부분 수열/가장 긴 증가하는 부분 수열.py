1475199a495c3cb2231e1ea1e9aff44359264968
2premise <43032391+JeonJe@users.noreply.github.com>
2022-11-07 11:26:36 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 136 ms, Memory: 114488 KB -BaekjoonHub

---
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.py
@@ -0,0 +1,17 @@
+n = int(input())
+arr = list(map(int,input().split()))
+arr.insert(0,0)
+dp = [0] * (1001)
+dp[1] = 1
+
+for i in range(2,n+1):
+    max_temp = 0
+
+    #현재 확인중인 인덱스보다 작은 인덱스들을 확인하여
+    #조건을 만족하는 가장 긴 부분수열의 값을 찾음
+    for k in range(i-1, 0, -1):
+        if arr[i] > arr[k]:
+            max_temp = max(max_temp,dp[k])
+    dp[i] = max_temp + 1
+
+print(max(dp))
