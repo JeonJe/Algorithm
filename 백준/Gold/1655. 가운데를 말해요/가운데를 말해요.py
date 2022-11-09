1d0c65c6931c020d44255ae97d2947cb461a9ff7
2premise <43032391+JeonJe@users.noreply.github.com>
2022-11-09 13:42:38 +0900

    [Gold II] Title: 가운데를 말해요, Time: 496 ms, Memory: 132472 KB -BaekjoonHub

---
+++ 백준/Gold/1655. 가운데를 말해요/가운데를 말해요.py
@@ -0,0 +1,36 @@
+import sys 
+import heapq
+
+n = int(input())
+
+min_heap = []
+max_heap = []
+
+f = int(input())
+if n == 1:
+    print(f)
+    exit()
+else:
+    heapq.heappush(max_heap,(-f,f))
+    print(f)
+
+
+for i in range(1,n):
+    f = int(sys.stdin.readline())
+    if len(max_heap) > len(min_heap):
+        heapq.heappush(min_heap, f)
+    else:
+        heapq.heappush(max_heap, (-f,f))
+    
+    if len(min_heap) > 0 and len(max_heap) > 0:
+        min_top = min_heap[0]
+        max_top = max_heap[0][1]
+
+        if min_top < max_top:
+            heapq.heappop(min_heap)
+            heapq.heappop(max_heap)
+            heapq.heappush(max_heap, (-min_top, min_top))
+            heapq.heappush(min_heap, max_top)
+        print(max_heap[0][1])
+        
+       
