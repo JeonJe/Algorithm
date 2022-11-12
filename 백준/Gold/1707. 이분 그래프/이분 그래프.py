83167083523c120cb4e6d1c82c57060d7df281b9
2premise <43032391+JeonJe@users.noreply.github.com>
2022-11-12 09:59:12 +0900

    [Gold IV] Title: 이분 그래프, Time: 1268 ms, Memory: 215564 KB -BaekjoonHub

--- 백준/Gold/1707. 이분 그래프/이분 그래프.py
+++ 백준/Gold/1707. 이분 그래프/이분 그래프.py
@@ -31,7 +31,6 @@ for _ in range(k):
     v, e = map(int,input().split())
     graph = [ [] for _ in range(v+1) ]
 
-
     for i in range(e):
         fr, to = map(int, input().split())
         graph[fr].append(to)
@@ -40,9 +39,11 @@ for _ in range(k):
     visited = [False] * (v+1)
     bipartite = [0]*(v+1)
     flag_bip = True
+
     for j in range(1,v+1):
         if visited[j] == False:
             if not bfs(j):
                 flag_bip = False
+                break
     print( 'YES' if flag_bip else'NO')
 
