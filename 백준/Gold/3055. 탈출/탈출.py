e61c9358395da24d379924a30ef2549cb0b16502
2premise <43032391+JeonJe@users.noreply.github.com>
2022-11-16 01:05:05 +0900

    [Gold IV] Title: 탈출, Time: 164 ms, Memory: 116516 KB -BaekjoonHub

--- 백준/Gold/3055. 탈출/탈출.py
+++ 백준/Gold/3055. 탈출/탈출.py
@@ -3,9 +3,7 @@ from collections import deque
 
 input = sys.stdin.readline
 R,C = map(int,input().split())
-
 graph = [ list(input().rstrip()) for _ in range(R)]
-
 
 dx = [ -1, 1, 0, 0]
 dy = [ 0, 0, 1, -1]
@@ -17,27 +15,24 @@ cnt = 0
 hedgehog = deque()
 water = deque()
 
-
-
 def bfs():
     global cnt 
-    while hedgehog:
+    while hedgehog: 
         for i in range(len(hedgehog)):
             hx, hy = hedgehog.popleft()
-
             for i in range(4):
                 nhx = hx + dx[i]
                 nhy = hy + dy[i]
-
                 if 0 <= nhx < R and 0 <= nhy < C:
                     if graph[nhx][nhy] == "D":
                         return True
-                    if (graph[nhx][nhy] == "."):
+                    elif (graph[nhx][nhy] == "."):
                         graph[nhx][nhy] = "S"
                         hedgehog.append((nhx,nhy))
         
         for i in range(len(water)):
             wx, wy = water.popleft()
+
             for j in range(4):
                 nwx = wx + dx[j]
                 nwy = wy + dy[j]
@@ -47,7 +42,6 @@ def bfs():
                             water.append((nwx,nwy))
                             if (nwx,nwy) in hedgehog:
                                 hedgehog.remove((nwx,nwy))
-
         cnt+=1
     return False
         
