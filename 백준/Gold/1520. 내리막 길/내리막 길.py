af4f11da7a4ecbc4a3a46443e68c70f9cb5ce524
2premise <43032391+JeonJe@users.noreply.github.com>
2022-11-24 16:57:41 +0900

    [Gold III] Title: 내리막 길, Time: 312 ms, Memory: 166068 KB -BaekjoonHub

--- 백준/Gold/1520. 내리막 길/내리막 길.py
+++ 백준/Gold/1520. 내리막 길/내리막 길.py
@@ -1,8 +1,6 @@
 
-
 #세로의 크기 M,
 #가로의 크기 N
-from collections import deque
 import sys
 input = sys.stdin.readline
 
@@ -34,34 +32,5 @@ def dfs(x,y):
                 dp[x][y] += dfs(nx,ny)
     return dp[x][y]
     
-
-#시간초과 dfs 
-# def dfs(x,y):
-#     stack = deque()
-#     stack.append((x,y))
-#     visited[x][y] = True
-    
-#     cnt = 0
-#     while stack:
-#         i, j = stack.pop()
-
-#         if i == M-1 and j == N-1:
-#             cnt+=1
-
-#         weight = graph[i][j] 
-#         for k in range(4):
-#             nx = i+dx[k]
-#             ny = j+dy[k]
-
-#             if 0<= nx < M and 0<= ny < N:
-#                 if graph[nx][ny] < weight:
-#                     stack.append((nx,ny))
-#                     visited[x][y] = True 
-#                     dp[nx][ny] += 1
-
-            
 dfs(0,0)
 print(dp[0][0])
-# for i in range(M):
-#     print(dp[i])
-# print(dp[M-1][N-1])
