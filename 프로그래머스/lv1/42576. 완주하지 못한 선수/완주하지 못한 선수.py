d5029a502131dede28da0049dda6eeb8da6b0c32
2premise <43032391+JeonJe@users.noreply.github.com>
2022-11-30 13:30:25 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 55.34 ms, Memory: 33.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -0,0 +1,22 @@
+def solution(participant, completion):
+    answer = ''
+    
+    dict = { }
+    
+    for p in participant:
+        if p not in dict:
+            dict[p] = 1
+        else:
+            dict[p] += 1
+            
+    for c in completion:
+        if dict[c] > 0:
+            dict[c] -= 1
+            if dict[c] == 0:
+                dict.pop(c)
+        else:
+            return c
+    key = dict.keys()
+    for i in key:
+        return i
+    
