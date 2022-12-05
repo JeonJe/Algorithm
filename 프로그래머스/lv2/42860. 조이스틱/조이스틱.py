3e1fd11045af245cfabb412c02f9b14279b854f7
2premise <43032391+JeonJe@users.noreply.github.com>
2022-12-05 20:09:00 +0900

    [level 2] Title: 조이스틱, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42860. 조이스틱/조이스틱.py
@@ -0,0 +1,17 @@
+def solution(name):
+    answer = 0 
+    lists = [ min(ord(s)-ord('A'),ord('Z')-ord(s) + 1) for s in name]
+    idx = 0
+    move = len(name) - 1
+    for i in range(len(lists)):
+        #스틱 위, 아래 중 최소값을 answer에 삽입 (=이동)
+        answer += lists[i]
+
+        idx = i+1
+        while(idx < len(lists) and lists[idx] == 0):
+            idx+=1
+        
+        # *2는 오고 가고,
+        move = min(move, i+len(lists)-idx+min(i,len(lists)-idx))
+        # move = min(move, (len(lists)-idx)*2+i)
+    return answer+move
