e668e057a0a508c9f480dca35f48e0e1c4caaac2
2premise <43032391+JeonJe@users.noreply.github.com>
2022-12-07 23:03:04 +0900

    [Silver II] Title: A → B, Time: 152 ms, Memory: 118120 KB -BaekjoonHub

---
+++ 백준/Silver/16953. A → B/A → B.py
@@ -0,0 +1,14 @@
+A,B = map(int,input().split())
+
+def dfs(x,cnt):
+    if x == B:
+        print(cnt)
+        exit()
+    if x > B:
+        return 
+    dfs(x*2,cnt+1)
+    next_num = int(str(x)+'1')
+    dfs(next_num,cnt+1)
+
+dfs(A,1)
+print(-1)
