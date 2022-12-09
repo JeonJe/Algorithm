36e4a10130dfb2cc9f1f83dd7db86464bd5e420e
2premise <43032391+JeonJe@users.noreply.github.com>
2022-12-09 21:36:49 +0900

    [unrated] Title: 디펜스 게임, Time: 0.17 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/142085. 디펜스 게임/디펜스 게임.py
@@ -0,0 +1,26 @@
+
+def check(enemy , n, k):
+    enemy.sort(reverse=True)
+    if k >= len(enemy):
+        return True 
+        
+    for i in range(k):
+        enemy[i] = 0
+    
+    if sum(enemy) <= n:
+        return True
+    else:
+        return False
+        
+def solution(n, k, enemy):
+    left = 0
+    right = len(enemy) + 1
+
+    while( left + 1 < right ):
+        mid = (left + right) // 2
+        if check(enemy[:mid], n, k):
+            left = mid 
+        else:
+            right = mid
+    
+    return left
