08aff0123597eb8541c4600a271ee4065efb3d8e
2premise <43032391+JeonJe@users.noreply.github.com>
2022-12-21 18:36:26 +0900

    [Bronze V] Title: 두 수 비교하기, Time: 0 ms, Memory: 1988 KB -BaekjoonHub

---
+++ 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int a, b;
+int main() {
+    cin >> a >> b;
+    
+    if(a>b) cout<<">";
+    if(a<b) cout<<"<";
+    if(a==b) cout<<"==";
+    return 0;
+}
