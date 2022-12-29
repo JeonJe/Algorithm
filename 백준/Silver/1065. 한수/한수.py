3722610111c551d492ba9b5b46f878016f4237f6
2premise <43032391+JeonJe@users.noreply.github.com>
2022-12-29 13:14:53 +0900

    [Silver IV] Title: 한수, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Silver/1065. 한수/한수.py
+++ 백준/Silver/1065. 한수/한수.py
@@ -1,24 +1,21 @@
-from collections import Counter
+n = int(input())
 
-N = int(input())
+cnt = 0
 
-
-cnt=0
-#한수 확인 범위 1부터 N까지
-for i in range(1, N+1):
-    result = []
-    s = str(i)
+def check(x):
+    digit = list(str(x))
     diff = 0
-    #한자리 수이면 등차 수열 
-    if len(s) < 2: 
-        result.append(s)
-       
-    else:
-        #두자리 이상 부터 각 자리 차이를 reulst에 넣음 
-        for j in range(1, len(s)):
-            diff = int(s[j-1]) - int(s[j])
-            result.append(diff)
-    #등차를 이루는 수가 1개이면 한수 
-    if len(Counter(result)) == 1:
-        cnt += 1
+    if len(digit) <= 1:
+        return True
+
+    diff = int(digit[0]) - int(digit[1])
+    for i in range(len(digit)-1):
+        if diff != int(digit[i]) - int(digit[i+1]):
+            return False
+    return True
+
+
+for i in range(1,n+1):
+    if check(i):
+        cnt += 1 
 print(cnt)
