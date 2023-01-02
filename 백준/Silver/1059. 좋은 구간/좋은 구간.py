289fab4d794b7db05fa22c5c76b8a7c93e48bd93
2premise <43032391+JeonJe@users.noreply.github.com>
2023-01-03 00:18:13 +0900

    [Silver IV] Title: 좋은 구간, Time: 108 ms, Memory: 113112 KB -BaekjoonHub

--- 백준/Silver/1059. 좋은 구간/좋은 구간.py
+++ 백준/Silver/1059. 좋은 구간/좋은 구간.py
@@ -1,39 +1,22 @@
 n = int(input())
 
-nums = list(map(int,input().split()))
-nums.insert(0,0)
-nums.insert(len(nums),1001)
+nums = list(map(int, input().split()))
 target = int(input())
-global res 
-res = []
-def in_range(n, start, end = 0):
-    return start <= n <= end if end >= start else end <= n <= start
 
-def check(left,right,target):
-    cnt = 0
-    global res
-    include = False
-    for i in range(left+1,right-1):
-        for j in range(i+1,right):
-            if i<=target<=j:
-                for num in nums:
-                    if in_range(num, i, j):
-                        include = True
-                        break
-                if not include:
-                    res.append((i,j))
+nums.sort()  # 두 수 사이에 있는 n을 찾아야 하므로 정렬
 
-                else:
-                    include = False
-
-
-for i in range(len(nums)-1):
-    left = nums[i]
-    right = nums[i+1]
-
-    if left < target < right:
-        check(left,right,target)
-
-temp = len(list(set(res)))
-# temp = len(res)
-print( temp if temp > 0 else 0 )
+if target in nums:
+    print(0)
+    
+else:
+    min = 0
+    max = 0
+    for num in nums:            # 배열중에서 n과 가장 근접한 두 수를 구한다.
+        if num < target:     
+            min = num
+        elif num > target and max == 0:
+            max = num
+    max -= 1                    # 1과 7사이에 n이 2이면 1과 7은 제외
+    min += 1
+    print((target-min)*(max-target+1) + (max-target))
+    # n보다 작은 수와 만족할 경우 + n보다 큰 수와 만족할 경우
