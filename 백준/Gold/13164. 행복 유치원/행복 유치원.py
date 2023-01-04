bfb1e5e05b36df5cbcb3b796d2520ed08c593c00
2premise <43032391+JeonJe@users.noreply.github.com>
2023-01-05 00:12:06 +0900

    [Gold V] Title: 행복 유치원, Time: 272 ms, Memory: 188400 KB -BaekjoonHub

---
+++ 백준/Gold/13164. 행복 유치원/행복 유치원.py
@@ -0,0 +1,11 @@
+N, K= map(int,input().split())
+
+t = list(map(int,input().split()))
+t.sort()
+
+diff = []
+for i in range(1,len(t)):
+    diff.append(t[i] - t[i-1])
+
+diff.sort(reverse=True)
+print(sum(diff[K-1:]))
