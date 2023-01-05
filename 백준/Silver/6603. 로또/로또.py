898fe5363e1c1345ee5dbe2cb25acc0b976532bc
2premise <43032391+JeonJe@users.noreply.github.com>
2023-01-05 17:00:58 +0900

    [Silver II] Title: 로또, Time: 148 ms, Memory: 116608 KB -BaekjoonHub

---
+++ 백준/Silver/6603. 로또/로또.py
@@ -0,0 +1,29 @@
+
+def dfs (depth,temp,S):
+   
+        
+    if len(temp) == 6:
+        print(*temp)
+        return 
+    if depth >= len(S):
+        return 
+    #depth 숫자를 선택하거나
+    
+    temp.append(S[depth])
+    dfs(depth+1,temp,S) 
+
+    temp.pop()
+    #dpeht의 숫자를 선택하지 않거나 
+    dfs(depth+1,temp,S)
+    return 
+
+while True :
+    commands = list(map(int,input().split()))
+    if commands[0]  == 0:
+        break 
+    
+    k = commands[0]
+    S = commands[1:]
+    temp = [] 
+    dfs(0,temp,S)
+    print()
