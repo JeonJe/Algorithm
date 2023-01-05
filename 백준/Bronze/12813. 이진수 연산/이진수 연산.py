45a75231b4421c552ce89f827cbfaa1226e6492b
2premise <43032391+JeonJe@users.noreply.github.com>
2023-01-05 12:01:18 +0900

    [Bronze II] Title: 이진수 연산, Time: 120 ms, Memory: 113248 KB -BaekjoonHub

--- 백준/Bronze/12813. 이진수 연산/이진수 연산.py
+++ 백준/Bronze/12813. 이진수 연산/이진수 연산.py
@@ -1,38 +1,12 @@
-A = list(map(int,list(input())))
-B = list(map(int,list(input())))
-# # A & B
-for i in range(len(A)):
-    if A[i] and B[i]:
-        print('1',end='')
-    else:
-        print('0',end='')
-print()
-#  A | B
-for i in range(len(A)):
-    if A[i] or B[i]:
-        print('1',end='')
-    else:
-        print('0',end='')
-print()
-#  A ^ B
-for i in range(len(A)):
-    if  (A[i] != B[i]) and (A[i] or B[i]):
-        print('1',end='')
-    else:
-        print('0',end='')
-print()
+A = int(input(),2) 
+B = int(input(),2)
+n = 100000
+mask = 2 ** n - 1
 
-#  ~A
-for i in range(len(A)):
-    if A[i]:
-        print('0',end='')
-    else:
-        print('1',end='')
-print()
-#  ~B
-for i in range(len(A)):
-    if B[i]:
-        print('0',end='')
-    else:
-        print('1',end='')
-print()
+# 자리수를 맞추기 위해 zfill 사용
+# 십진수끼리 & 비트연산자결 계산 결과 십진수 -> 이진수 변환 
+print(bin(A&B)[2:].zfill(n))  
+print(bin(A|B)[2:].zfill(n))
+print(bin(A^B)[2:].zfill(n))
+print(bin(A^mask)[2:].zfill(n))
+print(bin(B^mask)[2:].zfill(n))
