e8e2e1b09d76c8502992226b41683e86b756af23
2premise <43032391+JeonJe@users.noreply.github.com>
2023-01-06 09:57:36 +0900

    [Bronze IV] Title: 전자레인지, Time: 116 ms, Memory: 113248 KB -BaekjoonHub

---
+++ 백준/Bronze/10162. 전자레인지/전자레인지.py
@@ -0,0 +1,18 @@
+
+n = int(input())
+T = [300,60,10]
+
+used = False
+buttons = [0,0,0]
+
+
+for i in range(len(T)):
+    if n - T[i] >= 0:
+        buttons[i] += n // T[i] 
+        n = n % T[i]
+
+if sum(buttons) <= 0 or n > 0:
+    print(-1)
+else:
+    print(*buttons)
+
