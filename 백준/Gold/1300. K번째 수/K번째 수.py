13b1d857da11dd19552b9e9d05c5ee5fa7a25d76
2premise <43032391+JeonJe@users.noreply.github.com>
2023-01-07 06:08:43 +0900

    [Gold II] Title: K번째 수, Time: 156 ms, Memory: 114328 KB -BaekjoonHub

---
+++ 백준/Gold/1300. K번째 수/K번째 수.py
@@ -0,0 +1,24 @@
+n = int(input())
+k = int(input())
+
+left = 0
+right = k 
+
+# 이진탐색으로 인덱스가 들어갈 위치를 찾는다.
+def lower_bound(left, right, target):
+
+    while left < right:
+        mid = (left + right) // 2
+        cnt = 0
+
+        for i in range(1,n+1):
+            cnt += min(mid//i, n)
+
+        if cnt < target:
+            left = mid + 1
+        else:
+            right = mid
+
+    print(right)
+
+lower_bound(left,right, k)
