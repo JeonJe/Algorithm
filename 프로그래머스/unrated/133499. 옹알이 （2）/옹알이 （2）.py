b7e24763637d2b3a524f4edc248b52ac58f0ccc5
2premise <43032391+JeonJe@users.noreply.github.com>
2023-01-16 15:01:35 +0900

    [unrated] Title: 옹알이 (2), Time: 0.17 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/133499. 옹알이 （2）/옹알이 （2）.py
@@ -0,0 +1,28 @@
+def check(word):
+    can_speak = ["aya", "ye", "woo", "ma"]
+    last_word = []
+    word = list(word)
+
+    i = 0
+    while i < len(word):
+        find = False
+        for speak in can_speak:
+            if ''.join(word[i:i+len(speak)]) == speak:
+                if last_word == speak:
+                    return False
+                last_word = speak
+                i+=len(speak)
+                find = True
+
+        if not find :
+            return False
+        
+    return True
+
+
+def solution(babbling):
+    res = 0
+    for b in babbling:
+        if check(b):
+            res+=1
+    return res
