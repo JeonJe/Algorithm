f61ba53679169aa3fa1992c1dbb1d25634e48495
2premise <43032391+JeonJe@users.noreply.github.com>
2023-01-16 21:53:30 +0900

    [unrated] Title: 푸드 파이트 대회, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/unrated/134240. 푸드 파이트 대회/푸드 파이트 대회.py
+++ 프로그래머스/unrated/134240. 푸드 파이트 대회/푸드 파이트 대회.py
@@ -5,25 +5,27 @@ def solution(food):
     right = deque()
     
     foods = ""
-    for i in range(len(food)):
-        foods += str(i) * int(food[i])
+    for i in range(1,len(food)):
+        foods += str(i) * int(food[i] // 2)
+    reverse = foods[::-1]
+    return foods + '0' + reverse
 
-    que = deque(foods)
+#     que = deque(foods)
     
-    while len(que) > 0:
-        if len(que) >= 2:
-            if que[0] != que[1]:
-                que.popleft()
-                continue
-            f = que.popleft()
-            s = que.popleft()
-            if f == s:
-                left.append(f)
-                right.append(s)
-        else:
-            que.popleft()
+#     while len(que) > 0:
+#         if len(que) >= 2:
+#             if que[0] != que[1]:
+#                 que.popleft()
+#                 continue
+#             f = que.popleft()
+#             s = que.popleft()
+#             if f == s:
+#                 left.append(f)
+#                 right.append(s)
+#         else:
+#             que.popleft()
     
-    right = list(reversed(right))
-    answer = ''.join(left)+'0'+''.join(right)
+#     right = list(reversed(right))
+#     answer = ''.join(left)+'0'+''.join(right)
         
     return answer
