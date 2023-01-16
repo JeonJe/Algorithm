36f47e57c5fc160af324d5b07714f3242d0fcc44
2premise <43032391+JeonJe@users.noreply.github.com>
2023-01-16 21:19:23 +0900

    [level 1] Title: 신고 결과 받기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv1/92334. 신고 결과 받기/신고 결과 받기.py
+++ 프로그래머스/lv1/92334. 신고 결과 받기/신고 결과 받기.py
@@ -1,30 +1,34 @@
 from collections import defaultdict 
 def solution(id_list, report, k):
-    answer = defaultdict(int)
-    ans = []
-    nums = defaultdict(int)
-    repo_info = defaultdict(list)
-    repo = defaultdict(list)
+    repoted_info = defaultdict(list)
+    repoter_info = defaultdict(list)
     
     #신고당한사람 - 신고한사람 리스트
     for i in range(len(report)):
         reporter, reported = report[i].split()
         
-        if reported not in repo[reporter]:
-            repo[reporter].append(reported)
+        if reported not in repoter_info[reporter]:
+            repoter_info[reporter].append(reported)
 
-        if reporter not in repo_info[reported]:
-            repo_info[reported].append(reporter)
+        if reporter not in repoted_info[reported]:
+            repoted_info[reported].append(reporter)
     
     #key는 신고당한사람, value는 신고한사람
-    for key, value in repo_info.items():
+    #몇번 신고당했는지 카운트
+    nums = defaultdict(int)
+    for key, value in repoted_info.items():
         nums[key] += len(value)
     
-    for key,value in repo.items():
+    #key는 신고한사람, value는 신고당한 사람들
+    #신고당한 사람이 정지상태이면 신고한사람 받은메일 1개 추가
+    reporting = defaultdict(int)
+    for key,value in repoter_info.items():
         for v in value:
             if nums[v] >= k:
-                answer[key] += 1 
+                reporting[key] += 1 
 
+    ans = []
     for i in id_list:
-        ans.append(answer[i])
+        ans.append(reporting[i])
+
     return ans
