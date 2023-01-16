d7c215cd69ec9d79c036d0024bee1debc1d866ac
2premise <43032391+JeonJe@users.noreply.github.com>
2023-01-16 16:34:52 +0900

    [level 1] Title: 삼총사, Time: 0.13 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv1/131705. 삼총사/삼총사.py
+++ 프로그래머스/lv1/131705. 삼총사/삼총사.py
@@ -1,30 +1,33 @@
 
-def dfs(depth, temp, visited,number):
-    global res
- 
-    if len(temp) == 3:
-        if sum(temp) == 0:
-            res += 1
-        return
-        
-    if depth >= len(number):
-        return
-
-    #선택 
-    temp.append(number[depth])
-    visited[depth] = True
-    dfs(depth+1, temp, visited,number)
-
-    #미선택 
-    temp.pop()
-    visited[depth] = False
-    dfs(depth+1, temp, visited,number)
 
 def solution(number):
+    def dfs(depth, temp):
+        global res
+
+        if len(temp) == 3 :
+            if sum(temp) == 0:
+                res += 1
+            return
+
+        if depth >= len(number):
+            return 
+
+
+        #선택 
+        temp.append(number[depth])
+        visited[depth] = True
+        dfs(depth+1, temp)
+
+        #미선택 
+        temp.pop()
+        visited[depth] = False
+        dfs(depth+1, temp)
+
+
     global res
     res = 0
     temp = []
     visited = [False] * (len(number))
-    dfs(0,temp,visited,number)
+    dfs(0,temp)
 
     return res
