ff63a0c8839edab2437dbc17e319354a6db25a07
2premise <43032391+JeonJe@users.noreply.github.com>
2023-01-23 10:48:49 +0900

    [unrated] Title: 푸드 파이트 대회, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/134240. 푸드 파이트 대회/푸드 파이트 대회.py
@@ -0,0 +1,30 @@
+from collections import deque
+
+def solution(food):
+    left = deque()
+    right = deque()
+    
+    foods = ""
+    for i in range(1,len(food)):
+        foods += str(i) * int(food[i])
+
+
+    que = deque(foods)
+    
+    while len(que) > 0:
+        if len(que) >= 2:
+            if que[0] != que[1]:
+                que.popleft()
+                continue
+            f = que.popleft()
+            s = que.popleft()
+            if f == s:
+                left.append(f)
+                right.append(s)
+        else:
+            que.popleft()
+	    
+    right = list(reversed(right))
+    answer = ''.join(left)+'0'+''.join(right)
+
+    return answer
