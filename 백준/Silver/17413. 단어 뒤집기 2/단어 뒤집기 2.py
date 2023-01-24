4fdfcbd9020648c33a9e69a3151ad5d5048a37dc
2premise <43032391+JeonJe@users.noreply.github.com>
2023-01-24 09:38:39 +0900

    [Silver III] Title: 단어 뒤집기 2, Time: 524 ms, Memory: 165664 KB -BaekjoonHub

---
+++ 백준/Silver/17413. 단어 뒤집기 2/단어 뒤집기 2.py
@@ -0,0 +1,27 @@
+ins = input()
+stack = ""
+answer = ""
+
+Tag = False
+for c in ins:
+    if c == "<":
+        Tag = True
+        answer += stack[::-1]
+        answer += c
+        stack = ""
+        continue
+    elif c == ">":
+        Tag = False
+        answer += c
+        continue
+    elif c == " ":
+        answer += stack[::-1] + " "
+        stack = ""
+        continue
+    else:
+        if Tag:
+            answer += c
+        else:
+            stack += c
+
+print(answer+stack[::-1])
