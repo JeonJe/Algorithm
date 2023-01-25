8c2190c3f7c2dedf590d799a3b653b32dfd2a437
2premise <43032391+JeonJe@users.noreply.github.com>
2023-01-25 09:24:02 +0900

    [Silver II] Title: 스택 수열, Time: 1992 ms, Memory: 132296 KB -BaekjoonHub

---
+++ 백준/Silver/1874. 스택 수열/스택 수열.py
@@ -0,0 +1,26 @@
+n = int(input())
+
+target = [int(input()) for _ in range(n)]
+infos = [ i for i in range(n+1) ]
+stack = []
+res = []
+
+
+for i in range(1,n+1):
+    
+    res.append('+')
+    stack.append(i)
+
+    while len(target) > 0 and len(stack) > 0 and stack[-1] == target[0] :        
+        res.append('-')
+        stack.pop()
+        target.pop(0)
+
+if len(stack) != 0:
+    print("NO")
+    exit()
+
+for operation in res:
+    print(operation)
+
+
