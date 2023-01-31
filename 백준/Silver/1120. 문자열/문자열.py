5e4fdec034c0273057343d541c3086ff5a981b46
2premise <43032391+JeonJe@users.noreply.github.com>
2023-01-31 20:35:41 +0900

    [Silver IV] Title: 문자열, Time: 112 ms, Memory: 113112 KB -BaekjoonHub

--- 백준/Silver/1120. 문자열/문자열.py
+++ 백준/Silver/1120. 문자열/문자열.py
@@ -1,14 +1,6 @@
 import sys
 X,Y = input().split()
 
-res = 0
-if len(X) == len(Y):
-    for i in range(len(X)):
-        if X[i] != Y[i]:
-            res +=1
-    print(res)
-    exit()
-
 res = sys.maxsize
 for i in range(len(Y)-len(X)+1):
     cnt = 0
