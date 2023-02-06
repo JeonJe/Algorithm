b94a95a4be83c233ee3f275c871ccd0ac6b8d280
2premise <43032391+JeonJe@users.noreply.github.com>
2023-02-06 22:41:07 +0900

    [unrated] Title: 둘만의 암호, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/unrated/155652. 둘만의 암호/둘만의 암호.py
+++ 프로그래머스/unrated/155652. 둘만의 암호/둘만의 암호.py
@@ -1,12 +1,11 @@
 
-
 def solution(s, skip, index):
     answer = ''
 
     alpha = [chr(i) for i in range(97,123)]
     for i in skip:
         alpha.remove(i)
-    alpha *= 3
+    alpha *= 4
 
     for a in s:
         answer += alpha[alpha.index(a)+index]
