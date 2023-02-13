07b5624edebe1b3b552dafbb7162d13b167ae8ce
2premise <43032391+JeonJe@users.noreply.github.com>
2023-02-13 14:09:03 +0900

    [unrated] Title: 호텔 대실, Time: 17.73 ms, Memory: 10.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/155651. 호텔 대실/호텔 대실.py
@@ -0,0 +1,10 @@
+from collections import defaultdict
+
+def solution(book_time):
+    dict = defaultdict(int)
+    for start, end in book_time:
+        s = int(start[:2])*60 + int(start[-2:])
+        e = int(end[:2])*60 + int(end[-2:])
+        for i in range(s, e+10):
+            dict[i] += 1
+    return max(dict.values())
