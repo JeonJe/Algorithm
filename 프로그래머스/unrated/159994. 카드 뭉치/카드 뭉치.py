37060dba14ab8f2d9652cce38286a570b4efaa29
2premise <43032391+JeonJe@users.noreply.github.com>
2023-02-19 17:15:13 +0900

    [unrated] Title: 카드 뭉치, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/159994. 카드 뭉치/카드 뭉치.py
+++ 프로그래머스/unrated/159994. 카드 뭉치/카드 뭉치.py
@@ -3,34 +3,24 @@ def solution(cards1, cards2, goal):
     turn = 0
     p, p1, p2 = 0, 0, 0
 
-    if goal[p] == cards1[p1]:
-        turn = 1
-    elif goal[p] == cards2[p2]:
-        turn = 2
-    else:
-        return "No"
-
-    #확인해야하는 단어가 남아 있을 경우 
+    Flag = False
     while p < len(goal):
-        if turn == 1:
-            if p1 >= len(cards1):
-                return "No"
-            
-            if goal[p] != cards1[p1]:
-                return "No"
-            
-            while p1 < len(cards1) and p < len(goal) and goal[p] == cards1[p1]:
-                p+=1
-                p1+=1
-            turn = 2
-        elif turn == 2:
-            if p2 >= len(cards2):
-                return "No"
-            if goal[p] != cards2[p2]:
-                return "No"
-            while p2 < len(cards2) and p < len(goal) and goal[p] == cards2[p2]:
-                p+=1
-                p2+=1
-            turn = 1
+    
+        while p1 < len(cards1) and p < len(goal) and goal[p] == cards1[p1]:
+            p+=1
+            p1+=1
+            Flag = True
+        
+        while p2 < len(cards2) and p < len(goal) and goal[p] == cards2[p2]:
+            p+=1
+            p2+=1
+            Flag = True
+        
+        if Flag:
+            Flag = False
+            continue
+        else:
+            return "No"
+                
 
     return answer
