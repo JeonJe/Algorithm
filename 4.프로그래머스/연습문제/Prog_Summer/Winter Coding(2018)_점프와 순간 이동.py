9c16a7af091aa8e3a24ad0560db118cadfe124c1
jeonje <whssodi@gmail.com>
2023-02-26 16:22:17 +0900

    R : Prog_Summer/winter coding(2018) 점프와 순간이동.py

---
+++ 4.프로그래머스/연습문제/Prog_Summer/Winter Coding(2018)_점프와 순간 이동.py
@@ -0,0 +1,23 @@
+def solution(n):
+    
+    def dfs(n):
+        global cnt 
+        if n == 0:
+            return 
+        if n % 2 == 0:
+            dfs(n//2)
+        else:
+            cnt+=1
+            dfs(n-1)
+
+
+    global cnt 
+    cnt = 0
+    if n == 1 or n== 2:
+        return 1
+    dfs(n)
+    return cnt
+
+N = 5000
+
+print(solution(N))
