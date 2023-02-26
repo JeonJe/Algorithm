cbc6058d611d40782dbcc035caf203ceef50b6be
jeonje <whssodi@gmail.com>
2023-02-26 10:54:02 +0900

    change : github directory clean up

---
+++ 4.프로그래머스/연습문제/프로그래머스_연습문제_이진변환.py
@@ -0,0 +1,18 @@
+def solution(s):
+    answer = [0,0]
+    
+    while s != "1" :
+        remove_zero_s = ''  
+        answer[0] += 1
+        for elem in s:
+            if elem == "1":
+                remove_zero_s += '1'
+            else:
+                answer[1] += 1
+        c = len(remove_zero_s)
+        s = bin(c)[2:]
+        
+    return answer
+
+s = "110010101001"
+print(solution(s))
--- 프로그래머스/unrated/134240. 푸드 파이트 대회/푸드 파이트 대회.py
+++ 프로그래머스/unrated/134240. 푸드 파이트 대회/푸드 파이트 대회.py
@@ -6,25 +6,26 @@ def solution(food):
     
     foods = ""
     for i in range(1,len(food)):
-        foods += str(i) * int(food[i])
+        foods += str(i) * int(food[i] // 2)
+    reverse = foods[::-1]
+    return foods + '0' + reverse
 
-
-    que = deque(foods)
+#     que = deque(foods)
     
-    while len(que) > 0:
-        if len(que) >= 2:
-            if que[0] != que[1]:
-                que.popleft()
-                continue
-            f = que.popleft()
-            s = que.popleft()
-            if f == s:
-                left.append(f)
-                right.append(s)
-        else:
-            que.popleft()
-	    
-    right = list(reversed(right))
-    answer = ''.join(left)+'0'+''.join(right)
-
+#     while len(que) > 0:
+#         if len(que) >= 2:
+#             if que[0] != que[1]:
+#                 que.popleft()
+#                 continue
+#             f = que.popleft()
+#             s = que.popleft()
+#             if f == s:
+#                 left.append(f)
+#                 right.append(s)
+#         else:
+#             que.popleft()
+    
+#     right = list(reversed(right))
+#     answer = ''.join(left)+'0'+''.join(right)
+        
     return answer
