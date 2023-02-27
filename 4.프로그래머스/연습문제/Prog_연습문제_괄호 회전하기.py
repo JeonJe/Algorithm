31d441de2f0e1f172331e6482d46681e88e779a3
jeonje <whssodi@gmail.com>
2023-02-27 13:09:18 +0900

    S : Prog_연습문제_괄호 회전하기

---
+++ 4.프로그래머스/연습문제/Prog_연습문제_괄호 회전하기.py
@@ -0,0 +1,38 @@
+
+
+def solution(s):
+    def isRight(target):
+        stack = []
+        leftside = ["[", "{", "("]
+
+        for i in target:
+            if i in leftside:
+                stack.append(i)
+            else:
+                if len(stack) == 0:
+                    return False
+                elif i == "]" and stack[-1] == "[":
+                    stack.pop()
+                elif i == "}" and stack[-1] == "{":
+                    stack.pop()
+                elif i == ")" and stack[-1] == "(":
+                    stack.pop()
+                else:
+                    return False
+        if len(stack) > 0 :
+            return False
+        return True
+
+    answer = 0
+    temp_s = list(s)
+    for i in range(len(temp_s)):
+        shift_s = temp_s[i:] + temp_s[:i]
+        if isRight(shift_s):
+            print(''.join(shift_s))
+            answer +=1
+
+    return answer
+
+
+s = "}]()[{"
+print(solution(s))
