cf2b31b4dfe4cfd4cc45003aba5051521c71fcac
jeonje <whssodi@gmail.com>
2023-03-08 00:16:17 +0900

    S : Prog_연습문제_바탕화면정리

--- 4.프로그래머스/연습문제/Prog_연습문제_n제곱배열자르기.py
+++ 4.프로그래머스/연습문제/Prog_연습문제_n제곱배열자르기.py
@@ -7,10 +7,9 @@ def solution(n, left, right):
     for i in range(left,right+1):
         row = i // n
         col = (i % n) 
-
         answer.append(max(row,col)+1)
     
-    return answer
+    return answer   
 
 
 n = 4
---
+++ 4.프로그래머스/연습문제/Prog_연습문제_바탕화면정리.py
@@ -0,0 +1,20 @@
+def solution(wallpaper):
+    answer = []
+    min_x,min_y,max_x,max_y = 1e9,1e9,0,0
+    
+    for i in range(len(wallpaper)):
+        for j in range(len(wallpaper[0])):
+            if wallpaper[i][j] == '#':
+                print(i,j)
+                min_x = min(min_x,i)
+                min_y = min(min_y,j)
+                max_x = max(max_x,i)
+                max_y = max(max_y,j)
+                
+    print(min_x,min_y,max_x+1,max_y+1)
+                
+    return answer
+
+
+wallpaper = ["..........", ".....#....", "......##..", "...##.....", "....#....."]
+print(solution(wallpaper))
