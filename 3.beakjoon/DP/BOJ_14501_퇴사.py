8fa3f1ebde41d430ac90138a0d89318e11cdf567
jeonje <whssodi@gmail.com>
2023-03-16 21:52:25 +0900

    R : BOJ_11561_징검다리

---
+++ 3.beakjoon/DP/BOJ_14501_퇴사.py
@@ -0,0 +1,19 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+Ti = [0]*(n+1)
+Pi = [0]*(n+1)
+
+for i in range(n):
+    t, p = map(int,input().split())
+    Ti[i] = t
+    Pi[i] = p
+
+dp = [0]*(n+1)
+for i in range(n-1,-1,-1):
+    if i + Ti[i] <= n:
+        dp[i] = max(Pi[i]+ dp[i+Ti[i]], dp[i+1])
+    else:
+        dp[i] = dp[i+1]
+print(max(dp))
