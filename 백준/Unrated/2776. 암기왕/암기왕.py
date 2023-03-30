602bdd0201c0d2aa37f5e537d5462fe043de45cc
2premise <43032391+JeonJe@users.noreply.github.com>
2023-03-30 21:50:30 +0900

    [Unrated] Title: 암기왕, Time: 1548 ms, Memory: 351236 KB -BaekjoonHub

---
+++ 백준/Unrated/2776. 암기왕/암기왕.py
@@ -0,0 +1,21 @@
+from collections import defaultdict 
+import sys 
+input = sys.stdin.readline
+T = int(input())
+
+for _ in range(T):
+    n = int(input())
+    dict = defaultdict(int)
+    arr1 = list(map(int,input().split()))
+    for i in arr1:
+        dict[i] += 1
+
+
+    M = int(input())
+    arr2 = list(map(int,input().split()))
+    for j in arr2:
+        if dict[j] == 0:
+            print(0)
+        else:
+            print(1)
+        
