33f9122ec072ed0da12bb2f0810101ea972875ec
2premise <43032391+JeonJe@users.noreply.github.com>
2023-04-01 11:50:14 +0900

    [Unrated] Title: 게임을 만든 동준이, Time: 112 ms, Memory: 113112 KB -BaekjoonHub

---
+++ 백준/Unrated/2847. 게임을 만든 동준이/게임을 만든 동준이.py
@@ -0,0 +1,11 @@
+n = int(input())
+answer = 0
+data = [ int(input()) for _ in range(n)]
+prev = data[-1]
+for i in range(len(data)-2,-1,-1):
+    if data[i] >= prev:
+        decre = data[i] - prev + 1
+        answer += decre
+        data[i] -= decre
+    prev = data[i]
+print(answer)
