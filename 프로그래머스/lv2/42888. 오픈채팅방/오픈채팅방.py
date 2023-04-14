87a8b802489ab1610bc006bb99c873c1fc387f3b
2premise <43032391+JeonJe@users.noreply.github.com>
2023-04-14 11:35:38 +0900

    [level 2] Title: 오픈채팅방, Time: 81.22 ms, Memory: 46.2 MB -BaekjoonHub

--- 프로그래머스/lv2/42888. 오픈채팅방/오픈채팅방.py
+++ 프로그래머스/lv2/42888. 오픈채팅방/오픈채팅방.py
@@ -2,29 +2,25 @@ from collections import defaultdict
 
 def solution(record):
     answer = []
+    temp = []
     dict = defaultdict(str)
-    actions = []
-    ids = []
     
     for i in range (len(record)):
         splited = record[i].split()
         action = splited[0]
         
         if action == "Enter":
-            actions.append(action)
-            ids.append(splited[1])  
+            temp.append((action,splited[1]))
             dict[splited[1]] = splited[2]
         elif action == "Leave":
-            actions.append(action)
-            ids.append(splited[1])
+            temp.append((action,splited[1]))
         else:
             dict[splited[1]] = splited[2]
             
-    for j in range(len(actions)):
-        if actions[j] == "Enter":
-            answer.append(f'{dict[ids[j]]}님이 들어왔습니다.')
+    for j in range(len(temp)):
+        if temp[j][0] == "Enter":
+            answer.append(f'{dict[temp[j][1]]}님이 들어왔습니다.')
         else:
-            answer.append(f'{dict[ids[j]]}님이 나갔습니다.')
+            answer.append(f'{dict[temp[j][1]]}님이 나갔습니다.')
             
-        
     return answer
