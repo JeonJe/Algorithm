1e19c362e48619cac2d780a53230ebd484cf872e
2premise <43032391+JeonJe@users.noreply.github.com>
2023-04-14 14:46:08 +0900

    [level 2] Title: [3차] 파일명 정렬, Time: 3.21 ms, Memory: 10.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.py
@@ -0,0 +1,38 @@
+from collections import defaultdict 
+
+def find_num_indexs(filename):
+    start_idx, end_idx = 0, 0
+    find_num = False
+    num_cnt = 0
+    for j in range(len(filename)):
+        if filename[j].isdigit() and not find_num:
+            find_num = True
+            start_idx = j
+            end_idx = j
+            num_cnt = 1
+        elif filename[j].isdigit() and find_num:
+            num_cnt+=1
+            end_idx = j
+            if num_cnt >= 5:
+                break
+        elif not filename[j].isdigit() and find_num:
+            end_idx = j-1
+            break 
+    return [start_idx, end_idx]
+    
+def solution(files):
+    answer = []
+    dict = defaultdict(list)
+    for i in range(len(files)):
+        file_name = files[i]
+        n_start_idx, n_end_idx = find_num_indexs(file_name)
+        HEAD = file_name[:n_start_idx].lower()
+        NUMBER = file_name[n_start_idx:n_end_idx+1]
+        dict[file_name] = [HEAD,NUMBER,i]
+    
+    sorted_dict = list(sorted(dict.items(), key= lambda x : (x[1][0],int(x[1][1]),x[1][2])))
+
+    for key, _ in sorted_dict:
+        answer.append(key)
+
+    return answer
