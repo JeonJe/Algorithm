80516d631743150f7c149bf0c9325342412cf7e7
2premise <43032391+JeonJe@users.noreply.github.com>
2023-04-20 12:55:54 +0900

    [Silver II] Title: 에디터, Time: 448 ms, Memory: 159708 KB -BaekjoonHub

--- 백준/Silver/1406. 에디터/에디터.py
+++ 백준/Silver/1406. 에디터/에디터.py
@@ -1,15 +1,9 @@
-from collections import deque 
 
 data = input()
 n = int(input())
 
-right_que = deque()
-
-left_stack = []
+left_stack = [i for i in data]
 right_stack = []
-
-for i in data:
-    left_stack.append(i)
 
 for i in range(n):
     command_str = input()
@@ -19,15 +13,13 @@ for i in range(n):
         left_stack.append(param)
     elif first_chr == "L":
         if len(left_stack) > 0:
-            right_que.appendleft(left_stack.pop())
+            right_stack.append(left_stack.pop())
 
     elif first_chr == "B":
         if len(left_stack) > 0:
             left_stack.pop()
     else:
-        if len(right_que) > 0:
-            left_stack.append(right_que.popleft())
+        if len(right_stack) > 0:
+            left_stack.append(right_stack.pop())
 
-answer = ''
-answer += "".join(left_stack) + "".join(right_que)
-print(answer)
+print("".join(left_stack) + "".join(reversed(right_stack)))
