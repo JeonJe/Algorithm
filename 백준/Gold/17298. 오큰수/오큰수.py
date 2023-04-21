70578b0c85b03ae62b2f51d5f6100225fa43ec79
2premise <43032391+JeonJe@users.noreply.github.com>
2023-04-21 16:44:27 +0900

    [Gold IV] Title: 오큰수, Time: 624 ms, Memory: 258252 KB -BaekjoonHub

---
+++ 백준/Gold/17298. 오큰수/오큰수.py
@@ -0,0 +1,17 @@
+n = int(input())
+seq = list(map(int,input().split()))
+stack = []
+
+answer = []
+for i in range(len(seq)-1,-1,-1):
+    while stack and stack[-1] <= seq[i]:
+        stack.pop()
+        
+    if not stack:
+        answer.append(-1)
+    else:
+        answer.append(stack[-1])
+    stack.append(seq[i])
+
+print(*reversed(answer))
+    
