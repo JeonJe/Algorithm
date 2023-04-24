618d175271cb3ef790052b4113fe268c1bbfd0ce
2premise <43032391+JeonJe@users.noreply.github.com>
2023-04-24 19:36:55 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 3822.15 ms, Memory: 43.7 MB -BaekjoonHub

--- 프로그래머스/lv2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.py
+++ 프로그래머스/lv2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.py
@@ -1,17 +1,4 @@
-from collections import defaultdict 
-
 def solution(elements):
-    elements*=2
-    sums = defaultdict(int)
-    #idx 부터 시작
-    for i in range(len(elements)//2):
-    
-        temp = 0
-        #idx+j개
-        for j in range(len(elements)//2):
-            temp += elements[i+j]
-            if temp not in sums:
-                sums[temp]+=1
-        
-        
-    return len(sums)
+    N = len(elements)
+    elements *= 2
+    return len(set(sum(elements[j:j+n]) for j in range(N) for n in range(1, N+1)))
