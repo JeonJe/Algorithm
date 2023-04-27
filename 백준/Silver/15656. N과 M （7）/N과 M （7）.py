74486ac7c0eae0e4a8411c1ce9cda4cf3e95c39b
2premise <43032391+JeonJe@users.noreply.github.com>
2023-04-27 11:34:30 +0900

    [Silver III] Title: N과 M (7), Time: 968 ms, Memory: 137888 KB -BaekjoonHub

---
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.py
@@ -0,0 +1,17 @@
+
+N,M = map(int,input().split())
+nums = list(map(int,input().split()))
+nums.sort()
+
+def dfs( path):
+    if len(path) >= M:
+        print(*path)
+        return 
+    
+    for i in range(len(nums)):
+        path.append(nums[i])
+        dfs(path)
+        path.pop()
+
+for i in range(len(nums)):
+    dfs([nums[i]])
