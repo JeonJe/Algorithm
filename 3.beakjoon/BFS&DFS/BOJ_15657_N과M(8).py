b51193b2deae2135b612cc5f501bffb24f7d09cd
jeonje <whssodi@gmail.com>
2023-04-30 22:47:36 +0900

    solved : ~230430

--- 3.beakjoon/BFS&DFS/BOJ_15657_N과M(8).py
+++ 3.beakjoon/BFS&DFS/BOJ_15657_N과M(8).py
@@ -1,17 +1,17 @@
-
-N,M = map(int,input().split())
-nums = [i for i in range(1,N+1)]
+N, M = map(int,input().split())
+nums = list(map(int,input().split()))
+nums.sort()
 
 def dfs(idx, path):
     if len(path) >= M:
         print(*path)
         return 
-
+    
     for i in range(len(nums)):
-        if idx <= nums[i]:
+        if path[-1] <= nums[i]:
             path.append(nums[i])
-            dfs(nums[i],path)
+            dfs(i,path)
             path.pop()
 
 for i in range(len(nums)):
-    dfs(nums[i], [nums[i]])
+    dfs(i,[nums[i]])
