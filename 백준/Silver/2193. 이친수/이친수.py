12cf506456e7928c1ef116d4749f0624b953b087
2premise <43032391+JeonJe@users.noreply.github.com>
2023-05-04 14:22:37 +0900

    [Silver III] Title: 이친수, Time: 116 ms, Memory: 113112 KB -BaekjoonHub

--- 백준/Silver/2193. 이친수/이친수.py
+++ 백준/Silver/2193. 이친수/이친수.py
@@ -1,13 +1,11 @@
-N = int(input())
+n = int(input())
 
-dp = [[0]*2 for _ in range(N+1)]
+dp = [0]*91
 
-if N == 1:
-    print(1)
-    exit()
-dp[1][1] = 1
-for i in range(2,N+1):
-    dp[i][0] += dp[i-1][0] + dp[i-1][1]
-    dp[i][1] += dp[i-1][0]
+dp[1] = 1
+dp[2] = 1
 
-print(sum(dp[N]))
+for i in range(3,91):
+    dp[i] = dp[i-1] + dp[i-2]
+
+print(dp[n])
