addba4902397efa82f422e001f773708c9bbf11c
2premise <43032391+JeonJe@users.noreply.github.com>
2023-05-09 00:25:04 +0900

    [Gold III] Title: 텀 프로젝트, Time: 2500 ms, Memory: 61632 KB -BaekjoonHub

--- 백준/Gold/9466. 텀 프로젝트/텀 프로젝트.py
+++ 백준/Gold/9466. 텀 프로젝트/텀 프로젝트.py
@@ -1,5 +1,5 @@
 import sys
-sys.setrecursionlimit(10**9)
+sys.setrecursionlimit(10**6)
 input = sys.stdin.readline 
 
 t = int(input())
@@ -7,32 +7,26 @@ t = int(input())
 def dfs(cur):
     global cnt 
     visited[cur] = True
+    cycle_list.append(cur)
     cur_want = pick[cur]
-
+    
     if not visited[cur_want]:
         dfs(cur_want)
     else:
-        if cur_want not in checked_member:
-            nex = cur_want
-            while nex != cur:
-                cnt+=1
-                nex = pick[nex]
-            cnt+=1
-    checked_member.add(cur)
-    return 
-
-
+        if cur_want in cycle_list:
+            cnt += len(cycle_list[cycle_list.index(cur_want) : ])    
+        return 
 
 
 for _ in range(t):
     n = int(input())
     pick = [0]+list(map(int,input().split()))
 
-    visited = [False for _ in range(n+1)]
-    checked_member = set()
+    visited = [False] *(n+1)
     cnt = 0
-    
+
     for i in range(1,n+1):
         if not visited[i]:
+            cycle_list = []
             dfs(i)
     print(n - cnt)
