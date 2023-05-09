a1feb4ef4a2e7849010ac618004f5811b113a509
2premise <43032391+JeonJe@users.noreply.github.com>
2023-05-09 09:32:15 +0900

    [Silver III] Title: 순열 사이클, Time: 340 ms, Memory: 117192 KB -BaekjoonHub

---
+++ 백준/Silver/10451. 순열 사이클/순열 사이클.py
@@ -0,0 +1,28 @@
+t = int(input())
+
+def dfs(cur):
+    global cnt 
+
+    visited[cur] = True
+    next = seq[cur]
+    cycle_list.append(cur)
+    
+    if not visited[next]:
+        dfs(next)
+    else:
+        if next in cycle_list:
+            cnt+=1
+        return 
+    
+for _ in range(t):
+    n = int(input())
+    seq = [0] + list(map(int,input().split()))
+    visited = [False] * (n+1)
+    
+    global cnt
+    cnt = 0
+    for i in range(1,n+1):
+        if not visited[i]:
+            cycle_list = []
+            dfs(i)
+    print(cnt)
