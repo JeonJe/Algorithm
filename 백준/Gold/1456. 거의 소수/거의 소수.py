e4cb1b0159c43e6721e5449a5b27d6ba88b03edc
2premise <43032391+JeonJe@users.noreply.github.com>
2023-05-16 21:21:38 +0900

    [Gold V] Title: 거의 소수, Time: 4884 ms, Memory: 193140 KB -BaekjoonHub

---
+++ 백준/Gold/1456. 거의 소수/거의 소수.py
@@ -0,0 +1,27 @@
+def eratos():
+    primes[0], primes[1] = False, False
+    n = len(primes)
+    for i in range(2, int(n**(1/2))+1):
+        for j in range(2*i,n,i):
+            primes[j] = False
+    
+
+A,B = map(int,input().split())
+primes = [True] * (int(B ** (1/2))+1)
+eratos()
+
+answer = 0
+for i in range(1, len(primes)):
+    if primes[i]:
+        temp = i * i 
+        while True:
+            if temp < A:
+                temp *= i 
+                continue
+            if temp > B:
+                break
+
+            answer += 1
+            temp *= i 
+    
+print(answer)
