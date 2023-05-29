0d8fef744271aa814b789114f4aae1f381c6a947
2premise <43032391+JeonJe@users.noreply.github.com>
2023-05-29 11:44:24 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 448 ms, Memory: 149016 KB -BaekjoonHub

---
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.py
@@ -0,0 +1,32 @@
+from collections import deque, defaultdict
+
+n, m = map(int,input().split())
+
+if n == m:
+    print(0)
+    exit(0)
+
+que = deque()
+que.append((n,0))
+visited = defaultdict(bool)
+
+left_limit = -300000
+right_limit = 300000
+
+while que:
+    cx, time = que.popleft()
+        
+    if cx == m:
+        print(time)
+        break
+
+    if not visited[cx]:
+        visited[cx] = True
+    if cx*2 <= right_limit and not visited[cx*2]:
+        que.append((cx*2, time))
+    if cx-1 >= left_limit and not visited[cx-2]:
+        que.append((cx-1, time+1))
+    if cx+1 <= right_limit and not visited[cx+1]:
+        que.append((cx+1, time+1))
+
+
