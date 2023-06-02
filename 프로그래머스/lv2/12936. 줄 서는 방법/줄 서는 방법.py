07600ecf34531c66421139ce22006988f81630da
2premise <43032391+JeonJe@users.noreply.github.com>
2023-06-02 14:41:51 +0900

    [level 2] Title: 줄 서는 방법, Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12936. 줄 서는 방법/줄 서는 방법.py
@@ -0,0 +1,19 @@
+import math
+    
+def solution(n, k):
+    answer = []
+    seq = [i for i in range(1, n+1)]
+
+    while n :
+      
+        temp = math.factorial(n) // n
+        q = k // temp
+        k = k % temp 
+
+        if k == 0:
+            answer.append(seq.pop(q-1))
+        else:
+            answer.append(seq.pop(q))
+        n -= 1
+    return answer
+      
