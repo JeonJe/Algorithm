fb4d8a0f22b6277405cbc09babc817e56f360c58
JeonJeLee <43032391+JeonJe@users.noreply.github.com>
2023-06-23 14:46:15 +0900

    [Silver II] Title: 꽃길, Time: 220 ms, Memory: 116060 KB -BaekjoonHub

--- 백준/Silver/14620. 꽃길/꽃길.py
+++ 백준/Silver/14620. 꽃길/꽃길.py
@@ -1,50 +1,50 @@
+
 import sys 
 input = sys.stdin.readline 
 
 n = int(input())
 board = [ list(map(int,input().split())) for _ in range(n) ]
+visited = [ [False for _ in range(n)] for _ in range(n) ]
+dx = [0,-1,0,1,0]
+dy = [0,0,1,0,-1]
 
-dx = [-1,0,1,0]
-dy = [0,1,0,-1]
+def is_right_position(x,y):
+    for i in range(5):
+        nx = x + dx[i]
+        ny = y + dy[i]
 
-def find_min_value(coordinates):
-    visited = [ [False for _ in range(n)] for _ in range(n) ]
+        if not (0 <= nx < n and 0 <= ny < n):
+            return False
+        
+        if visited[nx][ny]:
+            return False
+        
+    return True
 
-    value = 0
-    for x,y in coordinates:
-        visited[x][y] = True
-        value += board[x][y]
-
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]        
-
-            if 0 <= nx < n and 0 <= ny < n:
-                if not visited[nx][ny]:
-                    value += board[nx][ny]
-                    visited[nx][ny] = True
-                else:
-                    return -1
-            else:
-                return -1
-    
-    return value
-
-
-def find_combinations(board, coordinates, depth):
+def find_points(board, coordinates, depth, value):
     if depth == 3:
-        result = find_min_value(coordinates)
-        if result != -1:
-            global answer
-            answer = min(answer, result)
+        global answer
+        answer = min(answer, value)
         return
     
     for i in range(len(board)):
         for j in range(len(board[i])):
-            if (i, j) not in coordinates:
 
-                find_combinations(board, coordinates + [(i,j)], depth+1)
-        
+            if is_right_position(i,j):
+                for k in range(5):
+                    nx = i + dx[k]
+                    ny = j + dy[k]
+                    visited[nx][ny] = True
+                    value += board[nx][ny]
+
+                find_points(board, coordinates + [(i,j)], depth+1, value)
+                
+                for k in range(5):
+                    nx = i + dx[k]
+                    ny = j + dy[k]
+                    visited[nx][ny] = False
+                    value -= board[nx][ny]
+
 answer = 1e9
-find_combinations(board, [], 0)
+find_points(board, [], 0, 0)
 print(answer)
