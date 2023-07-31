557e23b99e3e40f083b3591762302e4d9969ff67
JeonJeLee <43032391+JeonJe@users.noreply.github.com>
2023-07-31 11:16:55 +0900

    [Silver III] Title: 등수 매기기, Time: 884 ms, Memory: 54920 KB -BaekjoonHub

---
+++ 백준/Silver/2012. 등수 매기기/등수 매기기.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+  public static void main(String[] args) throws Exception {
+    BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+    int n = Integer.parseInt(reader.readLine());
+    int[] seq = new int[n];
+    for (int i = 0; i < n; i++) {
+      seq[i] = Integer.parseInt(reader.readLine());
+    }
+
+    Arrays.sort(seq);
+    long answer = 0L;
+
+    for (int i = 0; i < seq.length; i++) {
+      answer += Math.abs(seq[i] - (i+1));
+    }
+
+    System.out.println(answer);
+
+  }
+}
