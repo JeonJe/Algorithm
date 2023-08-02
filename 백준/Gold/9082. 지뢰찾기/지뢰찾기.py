1810a97111a51c377d33234b921b2c3262315e89
JeonJeLee <43032391+JeonJe@users.noreply.github.com>
2023-08-02 10:11:55 +0900

    [Gold V] Title: 지뢰찾기, Time: 112 ms, Memory: 113112 KB -BaekjoonHub

---
+++ 백준/Gold/9082. 지뢰찾기/지뢰찾기.py
@@ -0,0 +1,26 @@
+t = int(input())
+
+for _ in range(t):
+  n = int(input())
+  first_row = list(map(int,input()))
+  second_row = list(input())
+  answer = 0 
+  for i in range(n):
+    if i == 0:
+      if first_row[i] != 0 and first_row[i+1] != 0:
+        first_row[i] -= 1
+        first_row[i+1] -= 1
+        answer += 1
+    elif i == n-1:
+      if first_row[i] != 0 and first_row[i-1] != 0:
+        first_row[i] -= 1
+        first_row[i-1] -= 1
+        answer += 1
+    else:
+      if first_row[i] != 0 and first_row[i-1] != 0 and first_row[i+1] != 0:
+        first_row[i] -= 1
+        first_row[i-1] -= 1
+        first_row[i+1] -= 1
+        answer += 1
+  print(answer)
+
