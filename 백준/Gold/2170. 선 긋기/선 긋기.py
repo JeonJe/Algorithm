9002f6f1e6cee55b518cc6c68b47d1b2eec4df63
JeonJeLee <43032391+JeonJe@users.noreply.github.com>
2023-08-09 13:48:09 +0900

    [Gold V] Title: 선 긋기, Time: 4872 ms, Memory: 243184 KB -BaekjoonHub

---
+++ 백준/Gold/2170. 선 긋기/선 긋기.py
@@ -0,0 +1,18 @@
+n = int(input())
+seq = [list(map(int,input().split())) for i in range(n)]
+
+seq.sort()
+
+prev_end = -1e9
+answer = 0
+for start,end in seq:
+    #선분이 겹치지 않는 경우 
+    if start >= prev_end:
+        answer += end - start
+        prev_end = end    
+    #선분이 겹치는 경우 
+    elif end > prev_end:
+        answer += end - prev_end
+        prev_end = end
+        
+print(answer)
