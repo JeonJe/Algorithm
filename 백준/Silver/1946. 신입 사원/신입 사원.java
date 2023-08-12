c1c144d661e71521bab530e49500684475725cca
JeonJeLee <43032391+JeonJe@users.noreply.github.com>
2023-08-12 09:38:15 +0900

    [Silver I] Title: 신입 사원, Time: 3476 ms, Memory: 307608 KB -BaekjoonHub

---
+++ 백준/Silver/1946. 신입 사원/신입 사원.java
@@ -0,0 +1,38 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+  public static void main(String[] args) throws Exception {
+    BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+    int T = Integer.parseInt(reader.readLine());
+
+    for (int i = 0; i < T; i++) {
+      int N = Integer.parseInt(reader.readLine());
+
+      // 2차원 배열
+      int[][] seq = new int[N][2];
+
+      for (int j = 0; j < N; j++) {
+        StringTokenizer st = new StringTokenizer(reader.readLine());
+        int k = 0;
+        while (st.hasMoreTokens()) {
+          seq[j][k] = Integer.parseInt(st.nextToken());
+          k++;
+        }
+      }
+      Arrays.sort(seq, (o1, o2) -> {
+          return o1[0] - o2[0];
+      });
+      int answer = 1;
+      int prev = seq[0][1];
+      for (int j = 1; j < N; j++) {
+        if (prev > seq[j][1]) {
+          prev = seq[j][1];
+          answer++;
+        }
+      }
+    //test case 종료
+      System.out.println(answer);
+    }
+  }
+}
