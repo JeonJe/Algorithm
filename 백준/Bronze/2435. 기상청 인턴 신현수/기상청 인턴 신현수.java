8c91f8f3effa074c807d15aab2c38ff041e12f52
JeonJeLee <43032391+JeonJe@users.noreply.github.com>
2023-08-13 22:23:44 +0900

    [Bronze I] Title: 기상청 인턴 신현수, Time: 124 ms, Memory: 14132 KB -BaekjoonHub

---
+++ 백준/Bronze/2435. 기상청 인턴 신현수/기상청 인턴 신현수.java
@@ -0,0 +1,38 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+  public static void main(String[] args) throws Exception {
+    BufferedReader rd = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = new StringTokenizer(rd.readLine());
+    int n=0;
+    int k=0;
+
+    n = Integer.parseInt(st.nextToken());
+    k = Integer.parseInt(st.nextToken());
+    
+    int[] seq = new int[n];
+
+    st = new StringTokenizer(rd.readLine());
+    for (int i = 0; i < n; i++) {
+      seq[i] = Integer.parseInt(st.nextToken());
+    }
+    int answer = -1234567;
+    int left = 0, right = 0;
+    int cnt = 0;
+    int sumTemp = 0;
+    while (left <= right && right < n) {
+      
+      while(cnt < k) {
+        sumTemp += seq[right];
+        right++;
+        cnt++;
+      }
+      answer = Math.max(answer, sumTemp);
+      sumTemp -= seq[left];
+      left++;
+      cnt--;
+    }
+    System.out.println(answer);
+  }
+}
