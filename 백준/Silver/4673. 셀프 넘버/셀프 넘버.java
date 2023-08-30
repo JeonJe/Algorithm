80f515db6ab86355600d98e37df33273da980c17
JeonJeLee <43032391+JeonJe@users.noreply.github.com>
2023-08-30 12:50:38 +0900

    [Silver V] Title: 셀프 넘버, Time: 128 ms, Memory: 13956 KB -BaekjoonHub

---
+++ 백준/Silver/4673. 셀프 넘버/셀프 넘버.java
@@ -0,0 +1,30 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+  static boolean[] check = new boolean[10036];
+  public static void main(String[] args) throws Exception {
+    for (int i = 1; i < 10001; i++) {
+      check[d(i)] = true;
+    }
+    StringBuilder sb = new StringBuilder();
+    for (int i = 1; i < 10001; i++) {
+      if (!check[i]) {
+        sb.append(i);
+        sb.append('\n');
+      }
+    }
+    System.out.println(sb.toString());
+
+  }
+  static int d(int n) {
+    int sumN = n;
+    while (n > 0) {
+      sumN = sumN + n % 10;
+      n = (int)(n / 10);
+    }
+    return sumN;
+  }
+
+
+}
