74967d6c859bb0d3de2e2934d8f00d2c6b94f1c7
JeonJeLee <43032391+JeonJe@users.noreply.github.com>
2023-09-05 16:47:52 +0900

    [Silver III] Title: 통계학, Time: 588 ms, Memory: 156172 KB -BaekjoonHub

---
+++ 백준/Silver/2108. 통계학/통계학.py
@@ -0,0 +1,27 @@
+from collections import Counter
+
+n = int(input())
+seq = list(int(input()) for _ in range(n))
+
+seq.sort()
+
+average = round(sum(seq) / len(seq))
+print(average)
+
+median = seq[(len(seq))//2-1] if n % 2 == 0 else seq[len(seq)//2]
+print(median)
+
+count = list(Counter(seq).most_common())
+max_freq = count[0][1]
+
+same_freq = []
+for num, freq in count:
+  if freq == max_freq:
+    same_freq.append(num)
+same_freq.sort()
+
+mode = same_freq[0] if len(same_freq) == 1 else same_freq[1]
+print(mode)
+
+range = seq[-1] - seq[0]
+print(range)
