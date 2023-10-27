db5c171a6b9455a8294ff91cdf073369c13276b1
JeonJe <43032391+JeonJe@users.noreply.github.com>
2023-10-27 09:23:39 +0900

    Time: 1210 ms (5.02%), Space: 27.5 MB (8.09%) - LeetHub

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -0,0 +1,19 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        length = len(prices)
+        l,r = 0,0
+        profit = 0
+        while l < length:
+            print(prices[l], prices[r])
+            #r포인터가 가리키는 값이 l포인터가 가리키는 값보다 크면
+            #r을 증가시킴
+            if prices[r]>= prices[l]:
+                r += 1
+                if r >= length:
+                    break
+            else:
+                l = r
+                r = l
+            profit = max(profit, prices[r] - prices[l])
+                
+        return profit
