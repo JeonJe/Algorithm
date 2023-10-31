e68852dc5375d79d12d7e324e3c245f0703aae64
JeonJe <43032391+JeonJe@users.noreply.github.com>
2023-10-31 09:23:49 +0900

    Time: 135 ms (29.69%), Space: 16.2 MB (81.17%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -7,12 +7,11 @@ class Solution:
         for r in range(len(s)):
             cnt[s[r]] = cnt.get(s[r], 0) + 1
             
-            
             #sliding window lenght - most freq chracter count > k
             while (r - l + 1) - max(cnt.values()) > k:
                 cnt[s[l]] -= 1
                 l += 1
-            print(r-l+1, max(cnt.values()), r,l)
+                
             res = max(res, r-l+1)    
             
         
