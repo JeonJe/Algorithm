7d8de45b28a2cbfada093e8b5cfc5f9648636141
JeonJe <43032391+JeonJe@users.noreply.github.com>
2023-11-06 23:19:15 +0900

    Time: 699 ms (5.00%), Space: 16.5 MB (9.96%) - LeetHub

---
+++ LeetCode/0039-combination-sum/0039-combination-sum.py
@@ -0,0 +1,21 @@
+class Solution:
+    def combinationSum(self, candidates, target):
+        def dfs(path, sum_path):
+            if sum_path == target:
+                sorted_path = path[:]
+                sorted_path.sort()
+                if sorted_path not in answer:
+                    answer.append(sorted_path)
+                return
+
+            if sum_path > target:
+                return
+
+            for i in range(len(candidates)):
+                path.append(candidates[i])
+                dfs(path, sum_path + candidates[i])
+                path.pop()
+
+        answer = []
+        dfs([], 0)
+        return list(answer)
