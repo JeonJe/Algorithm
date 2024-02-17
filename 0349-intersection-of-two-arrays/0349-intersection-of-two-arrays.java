0577ace90420a92a0bc712a780c9cf70d1e3e10b
JeonJe <43032391+JeonJe@users.noreply.github.com>
2024-02-17 15:48:32 +0900

    Time: 5 ms (22.08%), Space: 43.1 MB (75.85%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.java
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.java
@@ -5,17 +5,26 @@ import java.util.Set;
 
 class Solution {
     public int[] intersection(int[] nums1, int[] nums2) {
-        Arrays.sort(nums1);
-
+        
         Set<Integer> answer = new HashSet<>();
 
-        for (int i : nums2) {
-            if (Arrays.binarySearch(nums1, i) >= 0) {
-                answer.add(i);
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+        int i = 0;
+        int j = 0;
+
+        while (i < nums1.length && j < nums2.length){
+            if(nums1[i] < nums2[j]){
+                i++;
+            } else if (nums1[i] > nums2[j]){
+                j++;
+            } else {
+                answer.add(nums1[i]);
+                i++;
+                j++;
             }
         }
         
-        Integer[] arr = answer.toArray(new Integer[0]);
-        return Arrays.stream(arr).mapToInt(Integer::intValue).toArray();
+        return answer.stream().mapToInt(Integer::intValue).toArray();
     }
 }
