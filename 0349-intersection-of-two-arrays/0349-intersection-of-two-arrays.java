d28342319fb401becef4d2a64d5cd518c939c14f
JeonJe <43032391+JeonJe@users.noreply.github.com>
2024-02-17 16:02:40 +0900

    Time: 6 ms (15.37%), Space: 43 MB (83.79%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.java
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.java
@@ -16,8 +16,16 @@ class Solution {
         while (i < nums1.length && j < nums2.length){
             if(nums1[i] < nums2[j]){
                 i++;
+                while(i < nums1.length && nums1[i-1] == nums1[i]){
+                    i++;
+                }
+                
             } else if (nums1[i] > nums2[j]){
                 j++;
+                
+                while(j < nums2.length && nums2[j-1] == nums2[j] ){
+                    j++;
+                }
             } else {
                 answer.add(nums1[i]);
                 i++;
