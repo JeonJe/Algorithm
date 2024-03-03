4cfe6919d3dc5295b31a867a3b29122115afce60
JeonJe <43032391+JeonJe@users.noreply.github.com>
2024-03-03 21:48:03 +0900

    Time: 3 ms (89.65%), Space: 43.2 MB (63.10%) - LeetHub

--- 1002-find-common-characters/1002-find-common-characters.java
+++ 1002-find-common-characters/1002-find-common-characters.java
@@ -1,21 +1,30 @@
 class Solution {
     public List<String> commonChars(String[] words) {
-        int[] alpha = new int[26];
-        Arrays.fill(alpha, Integer.MAX_VALUE);
+          int numOfAlphabet = 26;
+        int[] freqCharInWords = new int[numOfAlphabet];
+        Arrays.fill(freqCharInWords, Integer.MAX_VALUE);
 
-        for(String word: words){
-            int[] freqInWord = new int[26];
-            for (char c : word.toCharArray()) {
-                freqInWord[c - 'a']++;
+        for(int i = 0; i < words.length; i++){
+            int[] freqCharInAlpha = new int[numOfAlphabet];
+            Arrays.fill(freqCharInAlpha, 0);
+
+            for(char c : words[i].toCharArray()){
+                int charToInt = (int)(c -'a');
+                freqCharInAlpha[charToInt]++;
             }
-            for (int i = 0 ; i < 26; i++){
-                alpha[i] = Math.min(alpha[i], freqInWord[i]);
+
+            for(int j = 0 ; j < freqCharInAlpha.length; j++){
+                freqCharInWords[j] = Math.min(freqCharInWords[j], freqCharInAlpha[j]);
             }
         }
         List<String> answer = new ArrayList<>();
-        for(int i = 0 ; i < 26; i++) {
-            for(int j  = 0; j < alpha[i]; j++){
-                answer.add(Character.toString(('a' + i)));
+        for(int i = 0 ; i < numOfAlphabet; i++ ){
+            if(freqCharInWords[i] > 0) {
+                while(freqCharInWords[i] > 0){
+                    answer.add( Character.toString (i + 'a'));
+                    freqCharInWords[i]--;
+                }
+
             }
         }
         return answer;
