45a84137cf9cfe613a8c8141b7f221b298a18caf
JeonJe <43032391+JeonJe@users.noreply.github.com>
2024-03-05 23:47:53 +0900

    Time: 10 ms (30.90%), Space: 46.4 MB (56.81%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -1,7 +1,7 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
+           List<int[]> nonOverLappingList = new ArrayList<>();
         Arrays.sort(intervals, Comparator.comparingInt(a -> a[0]));
-        List<int[]> answer = new ArrayList<>();
 
         int start = intervals[0][0];
         int end = intervals[0][1];
@@ -9,21 +9,21 @@ class Solution {
         for (int i = 1 ; i < intervals.length; i++){
             //현재 시작 값이 이전 끝 값보다 크다면 이전 값은 추가 되어야 함
             if(intervals[i][0] > end) {
-                answer.add(new int[]{start, end});
+                nonOverLappingList.add(new int[]{start, end});
                 start = intervals[i][0];
                 end = intervals[i][1];
             } else if (intervals[i][1] > end){
                 end = intervals[i][1];
             }
         }
-        int answerSize = answer.size();
-       if(answerSize == 0 || (start > answer.get(answerSize-1)[1])){
-            answer.add(new int[]{start, end});
+        int nonOverLappingSize = nonOverLappingList.size();
+        if(nonOverLappingSize == 0 || (start > nonOverLappingList.get(nonOverLappingSize-1)[1])){
+            nonOverLappingList.add(new int[]{start, end});
         }
 
-        int[][] resultArray = new int[answer.size()][];
-        for(int i = 0 ; i < answer.size(); i++){
-            resultArray[i] = answer.get(i);
+        int[][] resultArray = new int[nonOverLappingList.size()][];
+        for(int i = 0; i < nonOverLappingList.size(); i++){
+            resultArray[i] = nonOverLappingList.get(i);
         }
         return resultArray;
     }
