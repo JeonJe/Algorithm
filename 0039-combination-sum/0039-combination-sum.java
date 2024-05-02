09b208d9f6b39038c1e448c196b15382c6c1194b
JeonJe <43032391+JeonJe@users.noreply.github.com>
2024-05-02 21:32:51 +0900

    Time: 3 ms (34.25%), Space: 44.2 MB (92.20%) - LeetHub

---
+++ 0039-combination-sum/0039-combination-sum.java
@@ -0,0 +1,26 @@
+class Solution {
+
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> answer = new ArrayList<>();  
+        Arrays.sort(candidates);
+
+        dfs(0, 0, new ArrayList<>(), candidates, target, answer);
+        return answer;
+    }
+
+    private void dfs(int depth, int sum, List<Integer> path, int[] candidates, int target, List<List<Integer>> answer) {
+        if (sum > target) {
+            return;
+        }
+        if(sum == target){
+            answer.add(new ArrayList<>(path));
+            return;
+        }
+
+        for(int i = depth; i < candidates.length; i++) {
+            path.add(candidates[i]);
+            dfs(i, sum + candidates[i], path, candidates, target, answer); 
+            path.remove(path.size() - 1);  
+        }
+    }
+}
