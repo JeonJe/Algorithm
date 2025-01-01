77f3c2fb06c7a153a7033e88b3f314d5bbca06de
jeonje <43032391+JeonJe@users.noreply.github.com>
2025-01-01 23:03:56 +0900

    Title: 자연수면 출력, Time: 138ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 250101/자연수면 출력/output-only-natural-number.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+
+public class Main {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+  
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        if(a <= 0) {
+            System.out.print("0");
+        } else {
+            for(int i =0; i < b; i++) {
+                System.out.printf("%d", a);
+            }
+        }
+
+
+
+    }
+}
