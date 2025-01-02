662055186f5ad0bfa92cce6b306bec03c1d1ae4c
jeonje <43032391+JeonJe@users.noreply.github.com>
2025-01-02 23:18:30 +0900

    Title: 별 찍는 것을 5번 반복하기, Time: 82ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 250102/별 찍는 것을 5번 반복하기/repeat-shooting-the-stars-five-times.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        for(int i = 0; i < 5; i++) {
+            print10Starts();
+        }
+    }
+
+    public static void print10Starts() {
+        for(int i = 0; i < 10; i++) {
+            System.out.print("*");
+        }
+        System.out.println();
+       
+    }
+}
