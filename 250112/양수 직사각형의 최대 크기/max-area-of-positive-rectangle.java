7eb2b071bbd3a460b55b27757286562c0f3097a1
jeonje <43032391+JeonJe@users.noreply.github.com>
2025-01-12 15:06:04 +0900

    Title: 양수 직사각형의 최대 크기, Time: 128ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 250112/양수 직사각형의 최대 크기/max-area-of-positive-rectangle.java
@@ -0,0 +1,66 @@
+
+import java.util.Scanner;
+
+class Main {
+    public static int n;
+    public static int m;
+    public static int maxCellCount;
+    public static int[][] board;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        m = sc.nextInt();
+
+        board = new int[n][m];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                board[i][j] = sc.nextInt();
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (board[i][j] > 0) {
+                    calculate(i, j);
+                }
+            }
+        }
+        System.out.println(maxCellCount);
+    }
+
+    private static void calculate(int x, int y) {
+
+        for (int i = x; i < n; i++) {
+            for (int j = y; j < m; j++) {
+                if (isAllPositive(x, i, y, j)) {
+                    maxCellCount = Math.max(maxCellCount, (i - x + 1) * (j - y + 1));
+                }
+            }
+        }
+
+        for (int j = y; j < m; j++) {
+            for (int i = x; i < n; i++) {
+                if (isAllPositive(x, i, y, j)) {
+                    maxCellCount = Math.max(maxCellCount, (i - x + 1) * (j - y + 1));
+                }
+            }
+        }
+    }
+
+
+    private static boolean isAllPositive(int rowStart, int rowEnd, int colStart, int colEnd) {
+        for (int i = rowStart; i <= rowEnd; i++) {
+            for (int j = colStart; j <= colEnd; j++) {
+                if (board[i][j] <= 0) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
+
+
+
