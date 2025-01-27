957cb1fe2656335d03c7f0ef4d6636d2bf0c7ae0
jeonje <43032391+JeonJe@users.noreply.github.com>
2025-01-27 11:50:01 +0900

    Title: 블럭쌓는 명령2, Time: 133ms, Memory: 10MB, Status: Passed - CodeTree

--- 250127/블럭쌓는 명령2/block-stacking-commands2.java
+++ 250127/블럭쌓는 명령2/block-stacking-commands2.java
@@ -6,7 +6,7 @@ public class Main {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
     int n = sc.nextInt();
-    blocks = new int[n];
+    blocks = new int[n+1];
 
     int k = sc.nextInt();
 
