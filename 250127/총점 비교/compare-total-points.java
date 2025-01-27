bbec61f5d15f62eda9f81b6384cd669ea461a74f
jeonje <43032391+JeonJe@users.noreply.github.com>
2025-01-27 10:33:51 +0900

    Title: 총점 비교, Time: 133ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 250127/총점 비교/compare-total-points.java
@@ -0,0 +1,49 @@
+import java.util.*;
+
+public class Main {
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+
+    Person[] persons = new Person[n];
+    for (int i = 0; i < n; i++) {
+      String name = sc.next();
+      int korean = sc.nextInt();
+      int english = sc.nextInt();
+      int math = sc.nextInt();
+
+      persons[i] = new Person(name, korean, english, math);
+    }
+
+    Arrays.sort(persons);
+
+    for (Person p : persons) {
+      System.out.printf("%s %d %d %d\n", p.name, p.korean, p.english, p.math);
+    }
+  }
+
+  public static class Person implements Comparable<Person> {
+
+    String name;
+    int korean;
+    int english;
+    int math;
+
+    public Person(String name, int korean, int height, int weight) {
+      this.name = name;
+      this.korean = korean;
+      this.english = height;
+      this.math = weight;
+    }
+
+    @Override
+    public int compareTo(Person o) {
+      return this.totalScore() - o.totalScore();
+    }
+
+    public int totalScore() {
+      return this.korean + this.english + this.math;
+    }
+  }
+}
