1da5c85170017ab6cf683b8bdfea4f83b544f0a4
jeonje <43032391+JeonJe@users.noreply.github.com>
2025-01-28 11:41:25 +0900

    Title: 겹치지 않는 사각형의 넓이, Time: 151ms, Memory: 26MB, Status: Runtime Error - CodeTree

---
+++ 250128/겹치지 않는 사각형의 넓이/area-of-non-overlapping-rectangle.java
@@ -0,0 +1,65 @@
+import java.util.*;
+
+public class Main {
+
+  public static final int MAX_BOARD_SIZE = 2001;
+  public static int[][] board = new int[MAX_BOARD_SIZE][MAX_BOARD_SIZE];
+  public static Squre A;
+  public static Squre B;
+  public static Squre M;
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    A = inputFrom(sc);
+    B = inputFrom(sc);
+    M = inputFrom(sc);
+
+    draw(A, 1);
+    draw(B, 1);
+    draw(M, 0);
+
+    int area = 0;
+    for (int i = 0; i < MAX_BOARD_SIZE; i++) {
+      for (int j = 0; j < MAX_BOARD_SIZE; j++) {
+        if (board[i][j] == 1) {
+          area++;
+        }
+      }
+    }
+    System.out.println(area);
+  }
+
+  private static void draw(Squre a, int num) {
+    for (int x = a.leftBottomX; x < a.rightTopX; x++) {
+      for (int y = a.leftBottomY; y < a.rightTopY; y++) {
+        board[x][y] = num;
+      }
+    }
+  }
+
+  private static Squre inputFrom(Scanner sc) {
+    int leftBottomX = sc.nextInt();
+    int leftBottomY = sc.nextInt();
+    int rightTopX = sc.nextInt();
+    int rightTopY = sc.nextInt();
+    return new Squre(leftBottomX, leftBottomY, rightTopX, rightTopY);
+  }
+
+  public static class Squre {
+
+    int leftBottomX;
+    int leftBottomY;
+    int rightTopX;
+    int rightTopY;
+
+    public Squre(int leftBottomX, int leftBottomY, int rightTopX, int rightTopY) {
+      this.leftBottomX = leftBottomX;
+      this.leftBottomY = leftBottomY;
+      this.rightTopX = rightTopX;
+      this.rightTopY = rightTopY;
+    }
+  }
+
+}
+
+
