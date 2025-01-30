9e48ce103dd4de8347f48c36997b52908d283dbf
jeonje <43032391+JeonJe@users.noreply.github.com>
2025-01-30 16:31:31 +0900

    Title: 만나는 그 순간, Time: 124ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 250130/만나는 그 순간/the-moment-we-meet.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+
+public class Main {
+
+  private static int MAX_TIME = 1000;
+  static int[] timeLineA = new int[MAX_TIME];
+  static int[] timeLineB = new int[MAX_TIME];
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int moveACount = sc.nextInt();
+    int moveBCount = sc.nextInt();
+
+    int currentTime = 1;
+    for (int i = 0; i < moveACount; i++) {
+      String direction = sc.next();
+      int time = sc.nextInt();
+      for (int j = currentTime; j < currentTime + time; j++) {
+        if ("R".equals(direction)) {
+          timeLineA[j] = timeLineA[j - 1] + 1;
+        } else {
+          timeLineA[j] = timeLineA[j - 1] - 1;
+        }
+      }
+      currentTime += time;
+    }
+
+    currentTime = 1;
+    for (int i = 0; i < moveBCount; i++) {
+      String direction = sc.next();
+      int time = sc.nextInt();
+      for (int j = currentTime; j < currentTime + time; j++) {
+        if ("R".equals(direction)) {
+          timeLineB[j] = timeLineB[j - 1] + 1;
+        } else {
+          timeLineB[j] = timeLineB[j - 1] - 1;
+        }
+      }
+      currentTime += time;
+    }
+
+    for (int i = 1; i < MAX_TIME; i++) {
+      if (timeLineA[i] == timeLineB[i]) {
+        System.out.println(i);
+        return;
+      }
+    }
+  }
+}
+
+
