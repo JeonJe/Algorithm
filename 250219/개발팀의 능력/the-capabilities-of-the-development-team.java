b933ab9cd25d271d5bb0983a8decdd6d7fd9dccc
jeonje <43032391+JeonJe@users.noreply.github.com>
2025-02-19 21:54:26 +0900

    Title: 개발팀의 능력, Time: 134ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 250219/개발팀의 능력/the-capabilities-of-the-development-team.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+public class Main {
+
+  static int[] arr = new int[5];
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int total = 0;
+    for (int i = 0; i < 5; i++) {
+      arr[i] = sc.nextInt();
+      total += arr[i];
+    }
+    int minDiff = Integer.MAX_VALUE;
+    for (int i = 0; i < 5; i++) {
+      for (int j = i + 1; j < 5; j++) {
+        for (int a = j + 1; a < 5; a++) {
+          for (int b = a + 1; b < 5; b++) {
+            int teamA = arr[i] + arr[j];
+            int teamB = arr[a] + arr[b];
+            int teamC = total - teamA - teamB;
+            if (teamA != teamB && teamB != teamC && teamA != teamC) {
+              minDiff = Math.min(minDiff, Math.max(teamA, Math.max(teamB, teamC)) - Math.min(teamA,
+                  Math.min(teamB, teamC)));
+            }
+          }
+        }
+      }
+    }
+    System.out.print(minDiff);
+  }
+
+}
