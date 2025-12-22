8a7934f6ce935b17a68f0c80936efb74fc7ad0f6
jeonje <43032391+JeonJe@users.noreply.github.com>
2025-12-22 13:40:23 +0900

    [Bronze II] Title: 블랙잭, Time: 104 ms, Memory: 14116 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.java
+++ 백준/Bronze/2798. 블랙잭/블랙잭.java
@@ -12,10 +12,11 @@ public class Main {
     int n = Integer.parseInt(st.nextToken());
     int m = Integer.parseInt(st.nextToken());
 
-    Integer[] nums = Arrays.stream(br.readLine().split(" "))
-        .map(Integer::parseInt)
-        .sorted()
-        .toArray(Integer[]::new);
+    int[] nums = new int[n];
+    st = new StringTokenizer(br.readLine());
+    for (int i = 0; i < n; i++) {
+      nums[i] = Integer.parseInt(st.nextToken());
+    }
 
     int numsLength = nums.length;
 
