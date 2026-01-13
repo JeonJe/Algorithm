facb75dfc19a62e43f86ccbe34c50831549d58a1
jeonje <43032391+JeonJe@users.noreply.github.com>
2026-01-13 21:42:53 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 92 ms, Memory: 14128 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
@@ -3,8 +3,6 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
-    private static int count = 0;
-
     public static void main(String[] args) throws Exception {
 //        System.setIn(new FileInputStream("input.txt"));  // 제출 시 이 줄만 주석처리
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -13,25 +11,21 @@ public class Main {
         for (int i = 1; i <= T; i++) {
             int target = Integer.parseInt(br.readLine());
 
-            count = 0;
-            dfs(1, target);
-            dfs(2, target);
-            dfs(3, target);
-            System.out.println(count);
+            int result = dfs(1, target) + dfs(2, target) + dfs(3, target);
+            System.out.println(result);
         }
     }
 
-    public static void dfs(int sum, int target) {
+    //sum에서 Target까지 가는 방법의 수
+    public static int dfs(int sum, int target) {
         if (sum > target) {
-            return;
+            return 0;
         }
         if (sum == target) {
-            count++;
-            return;
+            return 1;
         }
 
-        dfs(sum + 1, target);
-        dfs(sum + 2, target);
-        dfs(sum + 3, target);
+        //1을 선택했을 때 target까지 가는 방법의 수 + 2을 선택했을 때 target까지 가는 방법의 수 + 3을 선택했을 때 target까지 가는 방법의 수
+        return dfs(sum + 1, target) + dfs(sum + 2, target) + dfs(sum + 3, target);
     }
 }
