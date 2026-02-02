ce6258b8849dff649e04fec0cff2b51330c0a5e3
jeonje <43032391+JeonJe@users.noreply.github.com>
2026-02-02 21:31:53 +0900

    [Bronze I] Title: 슈퍼 마리오, Time: 104 ms, Memory: 14220 KB -BaekjoonHub

---
+++ 백준/Bronze/2851. 슈퍼 마리오/슈퍼 마리오.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        //System.setIn(new FileInputStream("input.txt"));  // 제출 시 이 줄만 주석처리
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int sum = 0;
+        int answer = 0;
+
+        for (int i = 0; i < 10; i++) {
+            sum += Integer.parseInt(br.readLine());
+            if (Math.abs(100 - sum) < Math.abs(100 - answer) ||
+                    (Math.abs(100 - sum) == Math.abs(100 - answer) && sum >= 100)) {
+                answer = sum;
+            }
+        }
+
+        System.out.println(answer);
+    }
+
+
+}
