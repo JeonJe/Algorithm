8474ff74ad5562e5bc8444190af3bf6ab7216dcb
jeonje <43032391+JeonJe@users.noreply.github.com>
2026-02-04 23:41:58 +0900

    [Bronze I] Title: 슈퍼 마리오, Time: 84 ms, Memory: 14160 KB -BaekjoonHub

--- 백준/Bronze/2851. 슈퍼 마리오/슈퍼 마리오.java
+++ 백준/Bronze/2851. 슈퍼 마리오/슈퍼 마리오.java
@@ -1,5 +1,5 @@
 import java.io.BufferedReader;
-import java.io.FileInputStream;
+//import java.io.FileInputStream;
 import java.io.InputStreamReader;
 import java.util.*;
 
@@ -14,13 +14,14 @@ public class Main {
 
         for (int i = 0; i < 10; i++) {
             sum += Integer.parseInt(br.readLine());
-            if (Math.abs(100 - sum) < Math.abs(100 - answer) ||
-                    (Math.abs(100 - sum) == Math.abs(100 - answer) && sum >= 100)) {
+            if (Math.abs(100 - sum) < Math.abs(100 - answer) || (sum >= 100 && Math.abs(100 - sum) == Math.abs(100 - answer))) {
                 answer = sum;
             }
-        }
 
+        }
         System.out.println(answer);
+
+
     }
 
 
