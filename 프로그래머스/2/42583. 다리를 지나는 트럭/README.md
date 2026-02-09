04af978178c95bdfa4d59e6901a168f305cc6ea6
jeonje <43032391+JeonJe@users.noreply.github.com>
2026-02-09 13:11:49 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 19.95 ms, Memory: 94.8 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/README.md
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/README.md
@@ -1,10 +1,10 @@
 # [level 2] 다리를 지나는 트럭 - 42583 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42583?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42583) 
 
 ### 성능 요약
 
-메모리: 83.3 MB, 시간: 21.65 ms
+메모리: 94.8 MB, 시간: 19.95 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 02월 06일 23:54:45
+2026년 02월 09일 13:11:47
 
 ### 문제 설명
 
--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
@@ -3,38 +3,41 @@ import java.util.*;
 class Solution {
     
     public int solution(int bridge_length, int weight, int[] truck_weights) {
-        int answer = 0;
         
-   		Queue<Integer> bridges = new LinkedList<>(); 
+        // 다리 만들기
+        Queue<Integer> bridge = new LinkedList<Integer>();
         for(int i = 0; i < bridge_length; i++) {
-            bridges.add(0);
+            bridge.add(0);
         }
         
         int time = 0;
         int currentWeight = 0;
-        //몇번째 트럭까지 보냈는지 
+        // 현재까지 몇 개의 트럭이 지나갔는지 표시 
         int idx = 0;
         
-        //보낼 트럭이 남아있거나, 아직 트럭이 다 내리지 못한 경우 
+        // 지나갈 트럭이 있거나, 다리 위에 트럭이 있는 동안
         while(idx < truck_weights.length || currentWeight > 0) {
+        // 시간이 지나고
             time++;
             
-            // 트럭이 끝에 도달했으면 내림 -> 큐의 마지막을 그냥 꺼내버림 
-            int out = bridges.poll();
+        // 다리 맨 끝 확인 
+            int out = bridge.poll();
             currentWeight -= out;
             
-            // 다음트럭이 올라올 수 있는 지 확인 
-        	if(idx < truck_weights.length) {
+        //다리 첫 번째 확인 
+            if(idx < truck_weights.length) {
+                // 새로운 트럭이 다리위에 올라감
                 if(currentWeight + truck_weights[idx] <= weight) {
-                    bridges.add(truck_weights[idx]);
                     currentWeight += truck_weights[idx];
+                    bridge.add(truck_weights[idx]);
                     idx++;
                 } else {
-                    bridges.add(0);
+                    bridge.add(0);
                 }
             }
         }
         
-        return time;
-    }
+    
+    	return time;
+	}	
 }
