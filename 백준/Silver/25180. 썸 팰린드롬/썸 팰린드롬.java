1ab3f5ce38c6461699315f29ad00b4b99d652da6
jeonje <43032391+JeonJe@users.noreply.github.com>
2026-02-25 23:29:02 +0900

    [Silver III] Title: 썸 팰린드롬, Time: 104 ms, Memory: 14352 KB -BaekjoonHub

---
+++ 백준/Silver/25180. 썸 팰린드롬/썸 팰린드롬.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        BufferedReader br = new BufferedReader(new FileReader("input.txt"));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+
+
+        // n = 15라면 한 자리(최대 값 9)로는 안되고 최소 2자리부터 시작
+        int k = (int) Math.ceil((double) n / 9);
+
+        while (true) {
+            //k
+            // 짝수 자릿수를 가지면, 홀수 N을 절대 만들 수 없음
+            if (k % 2 == 0 && n % 2 != 0) {
+                k++;
+            } else {
+                System.out.println(k);
+                break;
+            }
+        }
+
+        br.close();
+    }
+}
