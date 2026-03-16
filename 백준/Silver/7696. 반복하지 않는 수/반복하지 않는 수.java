104a24c92378c4480ad010e6d4637b732faadc05
jeonje <43032391+JeonJe@users.noreply.github.com>
2026-03-16 21:54:07 +0900

    [Silver III] Title: 반복하지 않는 수, Time: 556 ms, Memory: 18068 KB -BaekjoonHub

--- 백준/Silver/7696. 반복하지 않는 수/반복하지 않는 수.java
+++ 백준/Silver/7696. 반복하지 않는 수/반복하지 않는 수.java
@@ -1,41 +1,78 @@
 import java.io.BufferedReader;
-  import java.io.InputStreamReader;
-  import java.util.Arrays;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.StringTokenizer;
 
-  public class Main {
+public class Main {
 
-      static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-      static boolean[] visited = new boolean[10];
-      static long[] list = new long[8_877_700];
-      static int listIdx = 0;
+    public static final int INT = 1_000_000;
+    public static final int LIMIT_COUNT = INT;
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringBuilder sb = new StringBuilder();
+    static StringTokenizer st;
 
-      public static void main(String[] args) throws Exception {
-          for (int i = 1; i <= 9; i++) {
-              visited[i] = true;
-              dfs(i);
-              visited[i] = false;
-          }
+    static final int INF = Integer.MAX_VALUE;
+    static final long LINF = Long.MAX_VALUE;
+    static int n;
+    static boolean[] visited;
 
-          Arrays.sort(list, 0, listIdx);
+    static int[] dx = {1, 0, -1, 0};
+    static int[] dy = {0, 1, 0, -1};
 
-          StringBuilder sb = new StringBuilder();
-          String input;
-          while (!(input = br.readLine()).equals("0")) {
-              int idx = Integer.parseInt(input);
-              sb.append(list[idx - 1]).append('\n');
-          }
-          System.out.print(sb);
-      }
+    static int r, c, k;
+    static int count;
 
-      private static void dfs(long num) {
-          list[listIdx++] = num;
-          if (num > 9_876_543_210L) return;
-          for (int i = 0; i <= 9; i++) {
-              if (!visited[i]) {
-                  visited[i] = true;
-                  dfs(num * 10 + i);
-                  visited[i] = false;
-              }
-          }
-      }
-  }
+    //숫자를 1개씩 써서 만들 수 있는 숫자 모음
+    private static List<Long> list = new ArrayList<>();
+
+    public static void main(String[] args) throws Exception {
+
+        StringBuilder sb = new StringBuilder();
+
+        visited = new boolean[10];
+
+        // 1부터 1_000_000 번까지 반복하지 않는 수를 만듬
+        int[] arr = new int[LIMIT_COUNT + 1];
+
+        int cur = 1;
+        int idx = 1;
+        while(idx <= 1_000_000) {
+            if (!isRepeat(cur)) {
+                arr[idx] = cur;
+                idx++;
+            }
+            cur++;
+        }
+
+        while (true) {
+            String input = br.readLine();
+            if (input.equals("0")) {
+                break;
+            }
+            int targetIndex = Integer.parseInt(input);
+            sb.append(arr[targetIndex]).append("\n");
+        }
+        System.out.println(sb);
+
+    }
+
+    private static boolean isRepeat(int num) {
+        for(int i = 0 ; i < 10; i++) {
+            visited[i] = false;
+        }
+
+        while (num > 0) {
+            int digit = num % 10;
+            if(visited[digit]) {
+                return true;
+            }
+            visited[digit] = true;
+            num /= 10;
+        }
+        return false;
+    }
+
+
+}
