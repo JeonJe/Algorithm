9c109f6d8db541d7934e2695a887727a22f57a7f
jeonje <43032391+JeonJe@users.noreply.github.com>
2026-03-19 22:10:43 +0900

    [Bronze II] Title: 블랙잭, Time: 112 ms, Memory: 14044 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.java
+++ 백준/Bronze/2798. 블랙잭/블랙잭.java
@@ -1,36 +1,58 @@
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.StringTokenizer;
 
 public class Main {
 
-  private static int maxSum = 0;
+    public static final int INT = 1_000_000;
+    public static final int LIMIT_COUNT = INT;
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringBuilder sb = new StringBuilder();
+    static StringTokenizer st;
 
-  public static void main(String[] args) throws Exception {
+    static final int INF = Integer.MAX_VALUE;
+    static final long LINF = Long.MAX_VALUE;
+    static int n;
+    static boolean[] visited;
 
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    StringTokenizer st = new StringTokenizer(br.readLine());
-    int n = Integer.parseInt(st.nextToken());
-    int m = Integer.parseInt(st.nextToken());
+    static int[] dx = {1, 0, -1, 0};
+    static int[] dy = {0, 1, 0, -1};
 
-    int[] nums = new int[n];
-    st = new StringTokenizer(br.readLine());
-    for (int i = 0; i < n; i++) {
-      nums[i] = Integer.parseInt(st.nextToken());
-    }
+    static int r, c, k;
+    static int count;
 
-    int numsLength = nums.length;
+    //숫자를 1개씩 써서 만들 수 있는 숫자 모음
+    private static List<Long> list = new ArrayList<>();
 
-    for (int i = 0; i < numsLength - 2; i++) {
-      for (int j = i + 1; j < numsLength - 1; j++) {
-        for (int k = j + 1; k < numsLength; k++) {
-          int sum = nums[i] + nums[j] + nums[k];
-          if (sum <= m) {
-            maxSum = Math.max(maxSum, sum);
-          }
+    public static void main(String[] args) throws Exception {
+
+        st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
         }
-      }
+
+        int max = 0;
+        for (int i = 0; i < n - 2; i++) {
+            for (int j = i + 1; j < n - 1; j++) {
+                for (int k = j + 1; k < n; k++) {
+                    if (arr[i] + arr[j] + arr[k] <= m) {
+                        max = Math.max(max, arr[i] + arr[j] + arr[k]);
+                    }
+                }
+            }
+        }
+
+        System.out.println(max);
+
     }
-    System.out.println(maxSum);
-  }
+
 
 }
