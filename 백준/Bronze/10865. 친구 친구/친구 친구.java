366bcead5fada70531e1faf53a793d1ea9bc5b96
jeonje <43032391+JeonJe@users.noreply.github.com>
2026-03-24 22:30:49 +0900

    [Bronze III] Title: 친구 친구, Time: 1312 ms, Memory: 256260 KB -BaekjoonHub

---
+++ 백준/Bronze/10865. 친구 친구/친구 친구.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringBuilder sb = new StringBuilder();
+    static StringTokenizer st;
+
+    public static void main(String[] args) throws Exception {
+        st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        List<Integer>[] graph = new ArrayList[n+1];
+
+        for(int i = 0; i <= n; i++) {
+            graph[i] = new ArrayList<>();
+        }
+
+        for(int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            graph[a].add(b);
+            graph[b].add(a);
+        }
+
+        for(int i = 1; i <= n; i++) {
+            System.out.println(graph[i].size());
+        }
+
+    }
+
+
+}
