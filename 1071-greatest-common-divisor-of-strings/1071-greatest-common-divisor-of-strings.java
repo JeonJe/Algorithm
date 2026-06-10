680f5704140a6974845758330ae8193510d7f0e5
jeonje <43032391+JeonJe@users.noreply.github.com>
2026-06-10 22:09:46 +0900

    Time: 145 ms (5.02%), Space: 47 MB (5.99%) - LeetHub

---
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class Solution {
+    public String gcdOfStrings(String str1, String str2) {
+        String answer = ""; 
+
+        for (int i = 0; i < str1.length(); i++) {
+            String cur = str1.substring(0, i + 1);
+            System.out.println(cur);
+            
+            String temp1 = str1;
+            String temp2 = str2;
+
+            String check1 = temp1.replace(cur, "");
+            String check2 = temp2.replace(cur, "");
+            
+            if(check1.equals("") && check2.equals("")) {
+                answer = cur;
+            }
+
+        }
+
+        return answer;
+    }
+}
