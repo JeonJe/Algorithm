c14e5ec6193ac68cf086ffd04574befdbe8c6134
jeonje <43032391+JeonJe@users.noreply.github.com>
2026-06-10 22:13:35 +0900

    Time: 52 ms (5.02%), Space: 47.2 MB (5.99%) - LeetHub

--- 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.java
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.java
@@ -7,11 +7,8 @@ class Solution {
         for (int i = 0; i < str1.length(); i++) {
             String cur = str1.substring(0, i + 1);
             
-            String temp1 = str1;
-            String temp2 = str2;
-
-            String check1 = temp1.replace(cur, "");
-            String check2 = temp2.replace(cur, "");
+            String check1 = str1.replace(cur, "");
+            String check2 = str2.replace(cur, "");
             
             if(check1.equals("") && check2.equals("")) {
                 answer = cur;
