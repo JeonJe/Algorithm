27e11730327f97fb0158af2c6438fcc70115d7ca
jeonje <43032391+JeonJe@users.noreply.github.com>
2026-06-30 22:28:19 +0900

    Time: 5 ms (3.18%), Space: 65.2 MB (66.55%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -15,11 +15,11 @@ class Solution {
         for (int i = 1; i < n; i++) {
             left[i] = left[i - 1] * nums[i - 1];
         }
-        System.out.println(Arrays.toString(left));
+        
         for (int i = n - 2; i >= 0; i--) {
             right[i] = right[i + 1] * nums[i + 1];
         }
-        System.out.println(Arrays.toString(right));
+     
 
         for (int i = 0; i < n; i++) {
             answer[i] = left[i] * right[i];
