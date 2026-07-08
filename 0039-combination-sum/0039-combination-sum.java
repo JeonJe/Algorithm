715d330caf98fcb875b792826066c178eb9ca9a3
jeonje <43032391+JeonJe@users.noreply.github.com>
2026-07-08 21:42:55 +0900

    Time: 2 ms (96.36%), Space: 45.9 MB (33.81%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -1,31 +1,30 @@
 import java.util.*;
 
 class Solution {
-    private List<List<Integer>> answer;
+    private static List<List<Integer>> answer;
 
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
         answer = new ArrayList<>();
 
-        // Arrays.sort(candidates);
-
         List<Integer> bucket = new ArrayList<>();
         int curSum = 0;
 
-        dfs(candidates, bucket, curSum, target, 0);
+        dfs(candidates, bucket, target, 0, 0);
 
         return answer;
     }
 
-    private void dfs(int[] candidates, List<Integer> bucket, int curSum, int target, int start) {
+    private void dfs(int[] candidates, List<Integer> bucket, int target, int startIdx, int curSum) {
         if (curSum == target) {
             answer.add(new ArrayList<>(bucket));
             return;
         }
-        if (curSum > target) return;
+        if (curSum > target)
+            return;
 
-        for (int i = start; i < candidates.length; i++) {
+        for (int i = startIdx; i < candidates.length; i++) {
             bucket.add(candidates[i]);
-            dfs(candidates, bucket, curSum + candidates[i], target, i);
+            dfs(candidates, bucket, target, i, curSum + candidates[i]);
             bucket.removeLast();
         }
 
