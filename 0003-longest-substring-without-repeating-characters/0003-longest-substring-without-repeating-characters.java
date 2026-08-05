59fa799246d103989aef4d4df343c27a4a6762b5
jeonje <43032391+JeonJe@users.noreply.github.com>
2026-08-05 22:00:30 +0900

    Time: 68 ms (9.34%), Space: 47.8 MB (6.35%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -3,25 +3,17 @@ import java.util.*;
 class Solution {
     public int lengthOfLongestSubstring(String s) {
 
-        if (s.isEmpty()) {
-            return 0;
-        }
-
-        int answer = 1;
+        int answer = 0;
         int left = 0;
 
         Set<Character> temp = new HashSet<>();
-        temp.add(s.charAt(0));
 
-        for (int right = 1; right < s.length(); right++) {
+        for (int right = 0; right < s.length(); right++) {
 
-            //새 문자가 set에 있으면?
-            if (temp.contains(s.charAt(right))) {
-                // 사라질때까지 s[left]를 set에서 빼며 left를 전진시킨다.
-                while (temp.contains(s.charAt(right))) {
-                    temp.remove(s.charAt(left));
-                    left++;
-                }
+            // 사라질때까지 s[left]를 set에서 빼며 left를 전진시킨다.
+            while (temp.contains(s.charAt(right))) {
+                temp.remove(s.charAt(left));
+                left++;
             }
 
             //set에 새문자를 넣고. 길이를 갱신한다
