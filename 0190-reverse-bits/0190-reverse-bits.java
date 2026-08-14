60f9ded2dccdae00adb999a7597e2edaf9135513
jeonje <43032391+JeonJe@users.noreply.github.com>
2026-08-14 23:51:25 +0900

    Time: 2 ms (9.02%), Space: 42.6 MB (34.37%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.java
+++ 0190-reverse-bits/0190-reverse-bits.java
@@ -1,12 +1,33 @@
 
+// class Solution {
+//     public int reverseBits(int n) {
+
+//         int answer = 0;
+//         for (int i = 0; i < 32; i++) {
+//             int bitFlag = (n >> i) & 1;
+//             answer += (bitFlag << (31 - i));
+//         }
+//         return answer;
+//     }
+// }
 class Solution {
     public int reverseBits(int n) {
-
         int answer = 0;
-        for (int i = 0; i < 32; i++) {
-            int bitFlag = (n >> i) & 1;
-            answer += (bitFlag << (31 - i));
+        Deque<Integer> stack = new ArrayDeque<>();
+
+        for(int i = 0; i<32; i++) {
+            stack.add(n % 2);
+            n /= 2;
         }
-        return answer;
+
+        int j = 0;
+        while(!stack.isEmpty()) {
+            int bit = stack.getLast();
+            stack.removeLast();
+            answer += bit * Math.pow(2, j); 
+            j += 1;
+        }
+
+        return answer;       
     }
 }
