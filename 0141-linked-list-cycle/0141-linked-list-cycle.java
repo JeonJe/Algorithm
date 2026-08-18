9d6ed0c01bd001d9764996c22cb0102c3fc5c759
jeonje <43032391+JeonJe@users.noreply.github.com>
2026-08-18 21:03:21 +0900

    Time: 0 ms (100%), Space: 46.7 MB (73.63%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.java
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -1,32 +1,16 @@
-import java.util.HashSet;
-import java.util.Set;
-
 class Solution {
 
     public boolean hasCycle(ListNode head) {
-        if (head == null) {
-            return false;
-        }
-
         ListNode slow = head;
-        ListNode fast = null;
-        if (head.next != null) {
-            fast = head.next.next;
-        }
+        ListNode fast = head;
 
-        while (fast != null) {
-            if (fast == slow) {
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if (slow == fast) {
                 return true;
             }
-
-            slow = slow.next;
-
-            if (fast.next != null && fast.next.next != null) {
-                fast = fast.next.next;
-            } else {
-                return false;
-            }
-
         }
         return false;
     }
